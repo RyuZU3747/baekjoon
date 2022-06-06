28406d538664cc81575a3cd3ced87adb2959bcf5
RyuZU3747 <66157106+RyuZU3747@users.noreply.github.com>
2022-06-06 18:32:04 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long int
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+   cout<<"         ,r'\"7"<<'\n';
+   cout<<"r`-_   ,'  ,/"<<'\n';
+   cout<<" \\. \". L_r'"<<'\n';
+   cout<<"   `~\\/"<<'\n';
+   cout<<"      |"<<'\n';
+   cout<<"      |"<<'\n';
+}
