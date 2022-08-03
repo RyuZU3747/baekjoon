a305756f1af9aaef3051c47b94be147852d06c97
RyuZU3747 <66157106+RyuZU3747@users.noreply.github.com>
2022-08-03 23:52:18 +0900

    [Silver I] Title: 타일 위의 대각선, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2168. 타일 위의 대각선/타일 위의 대각선.cc
+++ 백준/Silver/2168. 타일 위의 대각선/타일 위의 대각선.cc
@@ -10,10 +10,7 @@ using namespace std;
 
 /*------------------------------------------------------------*/
 
-int gcd(int a, int b){
-    if(a%b==0) return b;
-    return gcd(b,a%b);
-}
+int gcd(int a, int b){return a%b==0 ? b : gcd(b,a%b);}
 
 int main(){
     int x,y;
