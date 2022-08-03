b2febfb3abdbacb8bae7af151a0385bc5876f7c7
RyuZU3747 <66157106+RyuZU3747@users.noreply.github.com>
2022-08-03 23:51:24 +0900

    [Silver I] Title: 타일 위의 대각선, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2168. 타일 위의 대각선/타일 위의 대각선.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+
+/*------------------------------------------------------------*/
+
+int gcd(int a, int b){
+    if(a%b==0) return b;
+    return gcd(b,a%b);
+}
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    int ans = x-1+y+1 - gcd(x,y);
+    cout<<ans;
+}
