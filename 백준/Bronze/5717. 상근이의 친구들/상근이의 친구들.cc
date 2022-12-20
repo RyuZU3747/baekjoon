565a100482340ed8840094cd4245bf9771be78fc
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2022-12-20 13:58:06 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<ll,ll>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    while(1){
+        int a, b;cin>>a>>b;
+        if(a==0&&b==0) break;
+        cout<<a+b<<'\n';
+    }
+}
