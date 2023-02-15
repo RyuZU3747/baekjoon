2c4216ea312da66e46d309edb3849f4fa8309e8c
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-02-16 01:43:39 +0900

    [Gold I] Title: 통신망 분할, Time: 28 ms, Memory: 5252 KB -BaekjoonHub

---
+++ 백준/Gold/17398. 통신망 분할/통신망 분할.cc
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int root[100010];
+ll su[100010];
+int chk[100010];
+ll ans;
+vector<pii> input;
+stack<int> st;
+
+int find(int x){
+    return root[x] = root[x] == x ? x : find(root[x]);
+}
+
+void Union(int x, int y){
+    int fx = find(x);
+    int fy = find(y);
+    if(fx>fy){
+        root[fx]=fy;
+        su[fy] += su[fx];
+        su[fx] = 0;
+    }
+    else{
+        root[fy]=fx;
+        su[fx] += su[fy];
+        su[fy] = 0;
+    }
+    return;
+}
+
+int main(){
+    FASTIO;
+    for(int i=0;i<100010;i++){
+        root[i]=i;
+        su[i] = 1;
+    }
+    int n,m,q;cin>>n>>m>>q;
+    if(n==1){
+        cout<<0;
+        return 0;
+    }
+    for(int i=0;i<m;i++){
+        int a,b;cin>>a>>b;
+        input.push_back({a,b});
+    }
+    for(int i=0;i<q;i++){
+        int a;cin>>a;
+        chk[a-1] = 1;
+        st.push(a-1);
+    }
+    for(int i=0;i<m;i++){
+        if(chk[i]==0){
+            int fs = find(input[i].first);
+            int se = find(input[i].second);
+            if(fs != se) Union(input[i].first,input[i].second);
+        }
+    }
+    while(!st.empty()){
+        int cur = st.top();
+        st.pop();
+        int fs = find(input[cur].first);
+        int se = find(input[cur].second);
+        if(fs != se){
+            ans += (su[fs] * su[se]);
+            Union(input[cur].first,input[cur].second);
+        }
+    }
+    cout<<ans;
+}
