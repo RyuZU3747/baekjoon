9b0975eb68e3cbfaff2d01dd26327409199cdda0
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-04-17 00:26:52 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int n,m;cin>>n>>m;
+    if(n*100 >= m) cout<<"Yes";
+    else cout<<"No";
+}
