817f9e0ae1102e83d33da88fba86835820f20e14
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-04-22 18:55:17 +0900

    [Bronze V] Title: Serca, Time: 124 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+string s = " @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n";
+
+int main(){
+    int n;cin>>n;
+    while(n--){
+        cout<<s;
+    }
+}
