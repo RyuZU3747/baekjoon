aaecedc41c2cc1ea1c5cb2fb726b0a6a1a390ff8
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-05-21 12:47:19 +0900

    [Platinum I] Title: 삶, 우주, 그리고 모든 것에 관한 궁극적인 질문의 해답, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Platinum/28123. 삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답/삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답.c
+++ 백준/Platinum/28123. 삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답/삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답.c
@@ -1,9 +1,1 @@
-int main(){
-    long long int n,k,x;
-    scanf("%lld %lld %lld",&n,&k,&x);
-    if(x==1) n--;
-    else if(x==2||x==3) n-=2;
-    else if(x==4||x==5||x==6||x==7) n-=3;
-    else n-=4;
-    printf("%lld",(n+1)-3*(k-1)+(x==4||x==8||x==9));
-}
+int main(){long long n,k,x;scanf("%lld %lld %lld",&n,&k,&x);if(x==1) n--;else if(x==2||x==3) n-=2;else if(x==4||x==5||x==6||x==7) n-=3;else n-=4;printf("%lld",(n+1)-3*(k-1)+(x==4||x==8||x==9));}
