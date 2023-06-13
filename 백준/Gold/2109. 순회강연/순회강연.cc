a5ea210ea6ee9873ca76063835afd87dde4a10b1
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-06-13 20:38:29 +0900

    [Gold III] Title: 순회강연, Time: 8 ms, Memory: 2196 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int root[10001];
+
+int find(int x){
+    return root[x] = root[x] == x ? x : find(root[x]);
+}
+
+void Union(int x, int y){
+    int fx = find(x);
+    int fy = find(y);
+    if(fx>fy) root[fx]=root[fy];
+    else root[fy]=root[fx];
+    return;
+}
+
+int main(){
+    for(int i=0;i<10001;i++) root[i] = i;
+    int n;cin>>n;
+    vector<pii> arr;
+    for(int i=0;i<n;i++){
+        int a,b;cin>>a>>b;
+        arr.push_back({a,b});
+    }
+    sort(arr.begin(),arr.end(),[](pii a, pii b){return a.first > b.first;});
+    int ans = 0;
+    for(auto e:arr){
+        int cur = find(e.second);
+        if(cur!=0){
+            ans += e.first;
+            Union(cur, cur-1);
+        }
+    }
+    cout<<ans;
+}
