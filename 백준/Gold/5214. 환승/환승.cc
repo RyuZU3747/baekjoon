d8ca718499fc0a0478682fb6c8b0f47461825600
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-06-14 22:16:32 +0900

    [Gold II] Title: 환승, Time: 500 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Gold/5214. 환승/환승.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+vector<int> gr[101001];
+int vis[101001];
+
+int main(){
+    int n,k,m;cin>>n>>k>>m;
+    for(int i=1;i<=m;i++){
+        for(int j=0;j<k;j++){
+            int a;cin>>a;
+            gr[a].push_back(n+i);
+            gr[n+i].push_back(a);
+        }
+    }
+    queue<iii> q;
+    q.push({1,1,-1});
+    vis[1] = 1;
+    int ans = -1;
+    while(!q.empty()){
+        int cur = get<0>(q.front());
+        int su = get<1>(q.front());
+        int bef = get<2>(q.front());
+        q.pop();
+        if(cur==n){
+            ans = max(ans, su);
+            break;
+        }
+        for(auto nxt:gr[cur]){
+            if(vis[nxt]) continue;
+            if(nxt > n){
+                q.push({nxt,su,cur});
+                vis[nxt] = 1;
+            }
+            else{
+                q.push({nxt,su+1,cur});
+                vis[nxt] = 1;
+            }
+        }
+    }
+    cout<<ans;
+}
