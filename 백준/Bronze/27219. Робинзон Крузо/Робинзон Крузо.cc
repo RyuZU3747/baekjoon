3e66d53e0c24cf555125b40bf7c481a647b1ce53
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-06-20 01:20:19 +0900

    [Bronze V] Title: Робинзон Крузо, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    for(int i=0;i<n/5;i++) cout<<"V";
+    for(int i=0;i<n%5;i++) cout<<"I";
+}
