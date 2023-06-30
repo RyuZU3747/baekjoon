e45f17eaa90bedcf9e4aab40ff5bd3305c009344
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-06-30 18:35:54 +0900

    [Platinum V] Title: 한동이는 영업사원!, Time: 16 ms, Memory: 13376 KB -BaekjoonHub

---
+++ 백준/Platinum/8012. 한동이는 영업사원！/한동이는 영업사원！.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int parent[100001][18];
+int depth[100001];
+int len[100001];
+vector<int> tree[100001];
+int n;
+
+int lca(int a, int b){
+	if(depth[a] < depth[b]) swap(a,b);
+	int dif = depth[a] - depth[b];
+
+	for(int i=0;dif;i++){
+		if(dif&1) a = parent[a][i];
+		dif >>= 1;
+	}
+	if(a!=b){
+		for(int j=17;j>=0;j--){
+			if(parent[a][j] != 0 && parent[a][j] != parent[b][j]){
+				a = parent[a][j];
+				b = parent[b][j];
+			}
+		}
+		a = parent[a][0];
+	}
+	return a;
+}
+
+void dfs(int cur){
+	for(auto e:tree[cur]){
+		if(depth[e]==-1){
+			parent[e][0] = cur;
+			depth[e] = depth[cur] + 1;
+			len[e] = len[cur] + 1;
+			dfs(e);
+		}
+	}
+}
+
+void init(){
+	for(int j=0;j<17;j++){
+		for(int i=1;i<=n;i++){
+			if(parent[i][j]!=0) parent[i][j+1] = parent[parent[i][j]][j];
+		}
+	}
+}
+
+int main(){
+	FASTIO;
+	cin>>n;
+	for(int i=0;i<n-1;i++){
+		int a,b;cin>>a>>b;
+		tree[a].push_back(b);
+		tree[b].push_back(a);
+	}
+	for(int i=0;i<100001;i++) depth[i] = -1;
+	depth[1] = 0;
+	dfs(1);
+	init();
+	int m;cin>>m;
+	vector<int> que;
+	int ans = 0;
+	for(int i=0;i<m;i++){
+		int a;cin>>a;
+		que.push_back(a);
+	}
+	for(int i=0;i<m-1;i++){
+		int f = que[i];
+		int s = que[i+1];
+		ans += len[f] + len[s] - 2*len[lca(f,s)];
+	}
+	cout<<ans;
+}
