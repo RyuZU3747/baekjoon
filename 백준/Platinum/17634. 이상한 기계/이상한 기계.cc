22ebe8113dbd2ceb577dfdf52d61e099454eac41
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2023-07-03 02:23:48 +0900

    [Platinum III] Title: 이상한 기계, Time: 568 ms, Memory: 26728 KB -BaekjoonHub

--- 백준/Platinum/17634. 이상한 기계/이상한 기계.cc
+++ 백준/Platinum/17634. 이상한 기계/이상한 기계.cc
@@ -1,94 +1,57 @@
 #include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
 using namespace std;
-#pragma GCC optimize("Ofast")
-#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
-#pragma GCC optimize("unroll-loops")
 
-#define fileio() freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
-#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define all(x) (x).begin(), (x).end()
-#define allr(x) x.rbegin(), x.rend()
-#define cmprs(x) sort(all(x)),x.erase(unique(all(x)),x.end())
-#define endl "\n"
-#define sp " "
-#define pb push_back
-#define lb lower_bound
-#define ub upper_bound
-#define F first
-#define S second
-#define rz resize
-#define sz(a) (int)(a.size())
-#define clr(a) a.clear()
+ll gcd(ll a, ll b){
+	if(b==0) return a;
+	return gcd(b, a%b);
+}
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<int, ll> pil;
-typedef tuple<int, int, int> tpi;
-typedef tuple<ll, ll, ll> tpl;
-typedef pair<double, ll> pdl;
-typedef pair<double, int> pdi;
+int main(){
+	FASTIO;
+	ll n,a,b;cin>>n>>a>>b;
+	ll p = a/gcd(a,1+b);
+	ll ans = 0;
+	if(INT_FAST64_MAX / b < p){
+		for(int i=0;i<n;i++){
+			ll l,r;cin>>l>>r;
+			ans += r-l+1;
+		}
+		cout<<ans;
+		return 0;
+	}
+	p *= b;
+	vector<pair<ll,ll>> ran; 
+	for(int i=0;i<n;i++){
+		ll l,r;cin>>l>>r;
+		if(r-l+1>=p){
+			cout<<p;
+			return 0;
+		}
+		l %= p;
+		r %= p;
+		if(l > r){
+			ran.push_back({l,p-1});
+			ran.push_back({0,r});
+		}
+		else ran.push_back({l,r});
+	}
+	sort(ran.begin(),ran.end());
+	ll l = -1, r=-1;
+	for(ll i=0;i<ran.size();i++){
+		if(r < ran[i].first){
+			ans += r-l+1;
+			l = ran[i].first;
+			r = ran[i].second;
+		}
+		r = max(r, ran[i].second);
+	}
+	ans += r-l+1;
+	cout<<ans-1;
 
-const int dx[] = {1,-1,0,0,1,1,-1,-1};
-const int dy[] = {0,0,1,-1,1,-1,1,-1};
-const ll MOD = 1e9+7;
-const ll INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3f;
-const int MAX = 101010; // PLZ CHK!
-
-int main() {
-    fio();
-
-    ll N,A,B;
-    cin>>N>>A>>B;
-
-    ll G=gcd(A,B+1);
-    ll T=A/G*B;
-
-    if (2ll*LINF/B<A/G) {
-        ll ans=0;
-        for (int i=0; i<N; i++) {
-            ll l,r; cin>>l>>r;
-            ans+=(r-l+1);
-        }
-        cout<<ans;
-        return 0;
-    }
-
-    vector<pll> v;
-    ll ans=0;
-    for (int i=0; i<N; i++) {
-        ll l,r;
-        cin>>l>>r;
-        if (T<=r-l+1) ans=T;
-
-        l%=T, r%=T;
-        if (l>r) {
-            v.pb({0,r});
-            v.pb({l,T-1});
-        }
-        else v.pb({l,r});
-    }
-    v.pb({LINF,LINF});
-    
-    if (ans) {
-        cout<<ans;
-        return 0;
-    }
-
-    sort(all(v));
-    
-    ll le=0, ri=-1;
-    for (auto [s,e]:v) {
-        if (ri<s) {
-            ans+=ri-le+1;
-            le=s, ri=e;
-        }
-        else {
-            ri=max(ri,e);
-        }
-    }
-
-    cout<<ans;
-    return 0;
 }
