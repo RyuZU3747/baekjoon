96f4ceeeaacec6592ea6507f4497fd6f952be261
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:19:29 +0900

    [Gold V] Title: 별꽃의 세레나데 (Easy), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/26217. 별꽃의 세레나데 （Easy）/별꽃의 세레나데 （Easy）.cc
+++ 백준/Gold/26217. 별꽃의 세레나데 （Easy）/별꽃의 세레나데 （Easy）.cc
@@ -1,12 +1,26 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+ll gcd(ll a,ll b){
+    if(a%b==0) return b;
+    else return gcd(b,a%b);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    ll up = 1, down = 1;
+    double ans = 0;
+    for(int i=1;i<=n;i++){
+        ans += (double)n/i;
+    }
+    cout<<fixed;
+    cout.precision(6);
+    cout<<ans;
+}
