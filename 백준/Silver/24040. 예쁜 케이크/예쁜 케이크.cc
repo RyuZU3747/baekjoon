2fdb01758ab7590ec84a0b46e603e3fdc4842e99
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:05 +0900

    [Silver I] Title: 예쁜 케이크, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/24040. 예쁜 케이크/예쁜 케이크.cc
+++ 백준/Silver/24040. 예쁜 케이크/예쁜 케이크.cc
@@ -1,12 +1,19 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long
+
+int main(){
+	int t;
+	ll int n;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+		scanf("%lld",&n);
+        if(n%3==2||n%9==0) printf("TAK\n");
+        else printf("NIE\n");
+	}
+}
