28daba9e7d5ee525b2c3c9f4d60dd160d0bcf645
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:24:19 +0900

    [Silver II] Title: 단어 우월 효과 (캠브릿지 대학의 연구결과), Time: 388 ms, Memory: 30940 KB -BaekjoonHub

--- 백준/Silver/25957. 단어 우월 효과 （캠브릿지 대학의 연구결과）/단어 우월 효과 （캠브릿지 대학의 연구결과）.cc
+++ 백준/Silver/25957. 단어 우월 효과 （캠브릿지 대학의 연구결과）/단어 우월 효과 （캠브릿지 대학의 연구결과）.cc
@@ -1,12 +1,27 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int n;cin>>n;
+    map<int, string> str;
+    map<string, int> schidx;
+    for(int i=0;i<n;i++){
+        string a; cin>>a;
+        str[i] = a;
+        if(a.length()>3) sort(a.begin()+1, a.end()-1);
+        schidx[a] = i;
+    }
+    int m;cin>>m;
+    for(int i=0;i<m;i++){
+        string a; cin>>a;
+        if(a.length()>3) sort(a.begin()+1, a.end()-1);
+        cout<<str[schidx[a]]<<" ";
+    }
+}
