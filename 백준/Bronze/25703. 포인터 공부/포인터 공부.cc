16bd214abbe53c7224c3c8a0649b4e2f68b1f742
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:54:04 +0900

    [Bronze III] Title: 포인터 공부, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25703. 포인터 공부/포인터 공부.cc
+++ 백준/Bronze/25703. 포인터 공부/포인터 공부.cc
@@ -1,12 +1,25 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+int main(){
+    int n;
+    cin>>n;
+    cout<<"int a;\n";
+    for(int i=0;i<n;i++){
+        cout<<"int ";
+        for(int j=0;j<=i;j++) cout<<"*";
+        if(i==0)cout<<"ptr"<<" = &a;\n";
+        else if(i==1) cout<<"ptr"<<i+1<<" = &ptr"<<";\n";
+        else{
+            cout<<"ptr"<<i+1<<" = &ptr"<<i<<";\n";
+        }
+    }
+}
