573f22e628b21fc736e2ce8500587a11b470b1ca
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:58:08 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -1,12 +1,18 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    map<string, string> a;
+    a["SONGDO"] = "HIGHSCHOOL";
+    a["CODE"] = "MASTER";
+    a["2023"] = "0611";
+    a["ALGORITHM"] = "CONTEST";
+    string b;cin>>b;
+    cout<<a[b];
+}
