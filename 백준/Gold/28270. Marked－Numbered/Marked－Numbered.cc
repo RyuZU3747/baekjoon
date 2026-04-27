eafeb80967e2a82c93bf93e8bda46512ae79d8f2
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:20:53 +0900

    [Gold V] Title: Marked-Numbered, Time: 532 ms, Memory: 16312 KB, Score: 3 point -BaekjoonHub

--- 백준/Gold/28270. Marked－Numbered/Marked－Numbered.cc
+++ 백준/Gold/28270. Marked－Numbered/Marked－Numbered.cc
@@ -1,12 +1,41 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    vector<int> stk;
+    vector<int> cnt(n);
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        int a;cin>>a;
+        stk.push_back(a);
+        cnt[a]++;
+        if(stk.size()==1){
+            if(a!=1){
+                cout<<-1;
+                return 0;
+            }
+            ans.push_back(1);
+        }
+        else{
+            if(stk[i] >= stk[i-1]){
+                if(stk[i] - stk[i-1] >= 2){
+                    cout<<-1;
+                    return 0;
+                }
+                ans.push_back(cnt[stk[i]]);
+            }
+            else{
+                for(int j=stk[i]+1;j<=stk[i-1];j++) cnt[j] = 0;
+                ans.push_back(cnt[stk[i]]);
+            }
+        }
+    }
+    for(auto e:ans) cout<<e<<' ';
+}
