6102fb2a999a1a43c612c54f05bc530e0beba18d
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:59:05 +0900

    [Gold II] Title: 재활용 캠페인, Time: 48 ms, Memory: 2640 KB -BaekjoonHub

--- 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.c
+++ 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.c
@@ -1,12 +1,50 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long
+
+int comp(const void *a,const void *b){
+	ll int c = *(ll int*)a;
+	ll int d = *(ll int*)b;
+	if(c>d) return 1;
+	else if(c==d) return 0;
+	else return -1;
+}
+
+
+int main(){
+	ll int n,x,c[110000];
+	scanf("%lld %lld", &n,&x);
+	ll int ans=0,cnt=0;
+	int p1=0,p2=n-1;
+	for(int i=0;i<n;i++){
+		scanf("%lld",&c[i]);
+	}
+	qsort(c,n,sizeof(ll int),comp);
+	while(p1<=p2){
+		if(c[p2]!=x) break;
+		ans+=1;
+		p2-=1;
+	}
+	while(p1<=p2){
+		ll int sum = c[p1]+c[p2];
+		if(p1==p2){
+			cnt+=1;
+			break;
+		}
+		if(sum*2>=x){
+			ans++;
+			p1++;
+			p2--;
+		}
+		else{
+			p1++;
+			cnt+=1;
+		}
+	}
+	printf("%lld\n",ans+cnt/3);
+}
