5c81d9c7d5776b6489eb04bd6cfe3f9e3216c1f4
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:22:55 +0900

    [Silver II] Title: Even More Odd Photos, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/20974. Even More Odd Photos/Even More Odd Photos.cc
+++ 백준/Silver/20974. Even More Odd Photos/Even More Odd Photos.cc
@@ -1,12 +1,33 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    int ar[1001];
+    int od = 0, ev = 0;
+    for(int i=0;i<n;i++){
+        cin>>ar[i];
+        if(ar[i]&1) od++;
+        else ev++;
+    }
+    int ans = 0;
+    if(od==ev||od+1==ev) ans = n;
+    else if(od<ev){
+        ans = od*2+1;
+    }
+    else{
+        while(od>ev){
+            od-=2;
+            ev++;
+        }
+        if(od==ev) ans = od*2;
+        else ans = od*2+1;
+    }
+    cout<<ans;
+}
