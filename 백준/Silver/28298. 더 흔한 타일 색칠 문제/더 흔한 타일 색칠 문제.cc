5441127a9a384ca08620fa00b84ce05102448169
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:28:26 +0900

    [Silver II] Title: 더 흔한 타일 색칠 문제, Time: 44 ms, Memory: 29420 KB -BaekjoonHub

--- 백준/Silver/28298. 더 흔한 타일 색칠 문제/더 흔한 타일 색칠 문제.cc
+++ 백준/Silver/28298. 더 흔한 타일 색칠 문제/더 흔한 타일 색칠 문제.cc
@@ -1,12 +1,50 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int cnt[501][501][27];
+
+int main(){
+    int n,m,k;cin>>n>>m>>k;
+    string tile[501];
+    for(int i=0;i<n;i++){
+        cin>>tile[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cnt[i%k][j%k][tile[i][j]-'A']++;
+        }
+    }
+    string pat[501];
+    for(int i=0;i<k;i++){
+        for(int j=0;j<k;j++){
+            int mxcnt = 0;
+            char mxc = 'A';
+            for(int x=0;x<27;x++){
+                if(cnt[i][j][x] > mxcnt){
+                    mxcnt = cnt[i][j][x];
+                    mxc = x+'A';
+                }
+            }
+            pat[i] += mxc;
+        }
+    }
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(tile[i][j] != pat[i%k][j%k]) ans++;
+        }
+    }
+    cout<<ans<<'\n';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<pat[i%k][j%k];
+        }
+        cout<<'\n';
+    }
+}
