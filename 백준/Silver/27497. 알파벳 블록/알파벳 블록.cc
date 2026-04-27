f431e5645c875fcb44d48c8f4938f2d33f3d8395
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:25:58 +0900

    [Silver II] Title: 알파벳 블록, Time: 308 ms, Memory: 7204 KB -BaekjoonHub

--- 백준/Silver/27497. 알파벳 블록/알파벳 블록.cc
+++ 백준/Silver/27497. 알파벳 블록/알파벳 블록.cc
@@ -1,12 +1,51 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+stack<char> fr;
+deque<char> rr;
+stack<int> whe;
+
+int main(){
+    int n;cin>>n;
+    for(int i=0;i<n;i++){
+        int a;cin>>a;
+        if(a==1){
+            char t;cin>>t;
+            whe.push(1);
+            rr.push_back(t);
+        }
+        else if(a==2){
+            char t;cin>>t;
+            whe.push(2);
+            fr.push(t);
+        }
+        else{
+            if(whe.empty()) continue;
+            else if(whe.top()==1){
+                whe.pop();
+                rr.pop_back();
+            }
+            else if(whe.top()==2){
+                whe.pop();
+                fr.pop();
+            }
+        }
+    }
+    if(fr.empty()&&rr.empty()) cout<<0;
+    else{
+        while(!fr.empty()){
+            cout<<fr.top();
+            fr.pop();
+        }
+        while(!rr.empty()){
+            cout<<rr.front();
+            rr.pop_front();
+        }
+    }
+}
