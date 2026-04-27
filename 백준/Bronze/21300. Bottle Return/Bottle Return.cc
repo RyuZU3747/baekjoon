396320ff277878ac6ece7ad4bc16d3adc1cd93c7
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:47:46 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/21300. Bottle Return/Bottle Return.cc
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -1,12 +1,19 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+
+/*------------------------------------------------------------*/
+
+int main(){
+    int a[6];
+    for(int i=0;i<6;i++) cin>>a[i];
+    int sum=0;
+    for(int i=0;i<6;i++) sum+=a[i];   
+    cout<<sum*5;
+}
