95a70a38b558cfcb0dc75351b43c6af3d754cf36
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:53:50 +0900

    [Bronze IV] Title: MBTI, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25640. MBTI/MBTI.cc
+++ 백준/Bronze/25640. MBTI/MBTI.cc
@@ -1,12 +1,24 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+ 
+int main(){
+    char s[5];
+    cin>>s;
+    int n;
+    cin>>n;
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        char tmp[5];
+        cin>>tmp;
+        if(strcmp(tmp,s)==0) cnt++;
+    }
+    cout<<cnt;
+}
