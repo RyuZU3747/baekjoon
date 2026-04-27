63a97ba931010f43b7adc3171b9d52a274aa24da
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:24:30 +0900

    [Silver V] Title: Identify, Sort, Index, Solve, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/26150. Identify， Sort， Index， Solve/Identify， Sort， Index， Solve.cc
+++ 백준/Silver/26150. Identify， Sort， Index， Solve/Identify， Sort， Index， Solve.cc
@@ -1,12 +1,32 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+vector<tuple<string, int, int>> ar;
+
+bool comp(tuple<string,int,int> a, tuple<string,int,int> b){
+    int x = get<1>(a);
+    int y = get<1>(b);
+    return x<y?1:0;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        string a;
+        int b,c;
+        cin>>a>>b>>c;
+        ar.push_back(make_tuple(a,b,c));
+    }
+    sort(ar.begin(),ar.end(),comp);
+    for(auto x: ar){
+        string tmp = get<0>(x);
+        cout<<(char)toupper(tmp[get<2>(x)-1]);
+    }
+}
