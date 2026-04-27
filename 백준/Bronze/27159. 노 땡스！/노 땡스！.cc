d6a1a465d6d4106fc033646d0172015c2c8ffc62
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:55:53 +0900

    [Bronze III] Title: 노 땡스!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27159. 노 땡스！/노 땡스！.cc
+++ 백준/Bronze/27159. 노 땡스！/노 땡스！.cc
@@ -1,12 +1,24 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int n;cin>>n;
+    int arr[34];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int ans = arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]-arr[i-1]>1){
+            ans += arr[i];
+        }
+    }
+    cout<<ans;
+}
