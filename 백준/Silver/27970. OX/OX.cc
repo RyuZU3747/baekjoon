db0b33b61cc32ae6c0dcff046a271478b8cf7894
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:27:40 +0900

    [Silver I] Title: OX, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/27970. OX/OX.cc
+++ 백준/Silver/27970. OX/OX.cc
@@ -1,12 +1,23 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    string s;cin>>s;
+    ll ans = 0;
+    ll idx = 1;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='O'){
+            ans += idx;
+            ans %= MOD;
+        }
+        idx *= 2;
+        idx %= MOD;
+    }
+    cout<<ans;
+}
