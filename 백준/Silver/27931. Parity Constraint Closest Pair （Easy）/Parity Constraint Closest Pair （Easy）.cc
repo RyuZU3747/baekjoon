0c29ba4fe91f435af3185e35f648c655224ab9a0
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:27:07 +0900

    [Silver III] Title: Parity Constraint Closest Pair (Easy), Time: 140 ms, Memory: 9324 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27931. Parity Constraint Closest Pair （Easy）/Parity Constraint Closest Pair （Easy）.cc
+++ 백준/Silver/27931. Parity Constraint Closest Pair （Easy）/Parity Constraint Closest Pair （Easy）.cc
@@ -1,12 +1,44 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int n;cin>>n;
+    vector<int> ev;
+    vector<int> od;
+    vector<int> al;
+    for(int i=0;i<n;i++){
+        int a;cin>>a;
+        if(abs(a)%2==1){
+            od.push_back(a);
+            al.push_back(a);
+        }
+        else{
+            ev.push_back(a);
+            al.push_back(a);
+        }
+    }
+    sort(ev.begin(),ev.end());
+    sort(od.begin(),od.end());
+    sort(al.begin(),al.end());
+    int odd = 2100000001;
+    int even = 2100000001;
+    for(int i=1;i<n;i++){
+        if(abs(al[i]-al[i-1]) % 2 == 1) odd = min(odd, abs(al[i]-al[i-1]));
+    }
+    for(int i=1;i<ev.size();i++){
+        even = min(even, abs(ev[i]-ev[i-1]));
+    }
+    for(int i=1;i<od.size();i++){
+        even = min(even, abs(od[i]-od[i-1]));
+    }
+    if(even == 2100000001) even = -1;
+    if(odd == 2100000001) odd = -1;
+    cout<<even<<' '<<odd;
+}
