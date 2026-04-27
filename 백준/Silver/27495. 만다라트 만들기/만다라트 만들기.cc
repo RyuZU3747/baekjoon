faa82347de0227035e6b2e8875dd95bb4c52bf8b
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:25:48 +0900

    [Silver III] Title: 만다라트 만들기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/27495. 만다라트 만들기/만다라트 만들기.cc
+++ 백준/Silver/27495. 만다라트 만들기/만다라트 만들기.cc
@@ -1,12 +1,42 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int dx[8] = {0,0,1,1,1,-1,-1,-1};
+int dy[8] = {-1,1,1,0,-1,1,-1,0};
+pair<int,int> st[8] = {{1,1},{1,4},{1,7},{4,1},{4,7},{7,1},{7,4},{7,7}};
+
+int main(){
+    vector<string> mp[9];
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            string tmp;cin>>tmp;
+            mp[i].push_back(tmp);
+        }
+    }
+    set<pair<string,set<string>>> man;
+    for(int i=0;i<8;i++){
+        set<string> tmp;
+        int x = st[i].first;
+        int y = st[i].second;
+        for(int j=0;j<8;j++){
+            tmp.insert(mp[x+dx[j]][y+dy[j]]);
+        }
+        man.insert({mp[x][y],tmp});
+    }
+    int idx = 1;
+    for(auto e:man){
+        cout<<"#"<<idx<<". "<<e.first<<'\n';
+        int i = 1;
+        for(auto x:e.second){
+            cout<<"#"<<idx<<"-"<<i<<". "<<x<<'\n';
+            i++;
+        }
+        idx++;
+    }
+}
