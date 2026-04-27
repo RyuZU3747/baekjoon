cd7c97e3d91960d9e2cda50c0a7821ea7632db63
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:48:08 +0900

    [Bronze V] Title: Multiply, Time: 156 ms, Memory: 113952 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.py
+++ 백준/Bronze/22193. Multiply/Multiply.py
@@ -1,12 +1,4 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+a,b=map(int,input().split())
+a = int(input())
+b = int(input())
+print(a*b)
