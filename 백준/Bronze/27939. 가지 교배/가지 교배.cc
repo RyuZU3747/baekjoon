a92434eb48697bd1fa616c1d86a70fe7ed87e739
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:57:16 +0900

    [Bronze I] Title: 가지 교배, Time: 312 ms, Memory: 6092 KB -BaekjoonHub

--- 백준/Bronze/27939. 가지 교배/가지 교배.cc
+++ 백준/Bronze/27939. 가지 교배/가지 교배.cc
@@ -1,12 +1,32 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    char pum[1001];
+    for(int i=0;i<n;i++) cin>>pum[i];
+    int m,k;cin>>m>>k;
+    map<int,vector<int>> josu;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<k;j++){
+            int a;cin>>a;
+            josu[i].push_back(a);
+        }
+    }
+    int cnt = 0;
+    for(int i=0;i<m;i++){
+        int no = 0;
+        for(auto e: josu[i]){
+            if(pum[e-1]=='P') no = 1;
+        }
+        if(no==0) cnt++;
+    }
+    if(cnt>0) cout<<'W';
+    else cout<<'P';
+}
