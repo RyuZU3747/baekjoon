24e1bd0356a5fb80ff2ef73fc30afea81e9e553b
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:55:40 +0900

    [Bronze III] Title: 성장의 비약 선택권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.cc
+++ 백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.cc
@@ -1,12 +1,41 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<ll,ll>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int mx = 4;
+    int lev = n+1;
+    int tmp = n;
+    if(tmp<=229){
+        tmp+=2;
+        if(tmp>230) tmp = 230;
+        if(lev<tmp){
+            mx = 3;
+            lev = tmp;
+        }
+    }tmp = n;
+    if(tmp<=219){
+        tmp+=4;
+        if(tmp>220) tmp = 220;
+        if(lev<tmp){
+            mx = 2;
+            lev = tmp;
+        }
+    }tmp = n;
+    if(tmp<=209){
+        tmp+=8;
+        if(tmp>210) tmp = 210;
+        if(lev<tmp){
+            mx = 1;
+            lev = tmp;
+        }
+    }
+    cout<<mx;
+}
