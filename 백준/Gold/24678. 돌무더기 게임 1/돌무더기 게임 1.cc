be362d383a3bcea5a5c3722c0c6d780a0e0cf36f
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:00:18 +0900

    [Gold II] Title: 돌무더기 게임 1, Time: 136 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/24678. 돌무더기 게임 1/돌무더기 게임 1.cc
+++ 백준/Gold/24678. 돌무더기 게임 1/돌무더기 게임 1.cc
@@ -1,12 +1,22 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long int
+using namespace std;
+
+int main(){
+	int t,x,y,z,a;
+	scanf("%d",&t);
+	while(t--){
+		scanf("%d %d %d",&x,&y,&z);
+		
+        a=0;
+        if(x%2==0) a++;
+        if(y%2==0) a++;
+        if(z%2==0) a++;
+        
+		if(a==2||a==3) printf("R\n");
+		else printf("B\n");
+	}
+}
