9a328cce2e6f4a1195b05a2867945aa119791c3d
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:26:02 +0900

    [Silver III] Title: 1차원 2048, Time: 152 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/27514. 1차원 2048/1차원 2048.cc
+++ 백준/Silver/27514. 1차원 2048/1차원 2048.cc
@@ -1,12 +1,26 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    map<ll, int> su;
+    for(int i=0;i<n;i++){
+        ll tmp;cin>>tmp;
+        su[tmp]++;
+    }
+    ll ans = 1;
+    for(ll i=1;i<=LLONG_MAX/2;i=i*2){
+        while(su[i]>=2){
+            su[i*2]++;
+            su[i]-=2;
+        }
+        if(su[i*2]>0) ans = i*2;
+    }
+    cout<<ans;
+}
