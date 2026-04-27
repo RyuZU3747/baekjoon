a5453071646f7a84b5477ae85c55201423edaa6e
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:27:17 +0900

    [Silver II] Title: 하이퍼 가지 따기, Time: 16 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Silver/27941. 하이퍼 가지 따기/하이퍼 가지 따기.cc
+++ 백준/Silver/27941. 하이퍼 가지 따기/하이퍼 가지 따기.cc
@@ -1,12 +1,40 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+map<vector<int>,int> ty;
+set<int> point[12];
+vector<int> tet;
+
+void backtrac(int len){
+    if(len==12){
+        if(ty[tet]==0){
+            for(auto x: tet) cout<<x<<' ';
+            exit(0);
+        }
+        return;
+    }
+    for(auto e: point[len]){
+        tet.push_back(e);
+        backtrac(len+1);
+        tet.pop_back();
+    }
+}
+
+int main(){;
+    for(int i=0;i<2047;i++){
+        vector<int> tmp;
+        for(int i=1;i<=11;i++){
+            int a;cin>>a;
+            tmp.push_back(a);
+            point[i].insert(a);
+        }
+        ty[tmp] = 1;
+    }
+    backtrac(1);
+}
