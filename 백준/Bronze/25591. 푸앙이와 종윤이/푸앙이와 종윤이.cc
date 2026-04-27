666c2dd22f961969faebafd98ca8b815fc526fb4
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:53:08 +0900

    [Bronze IV] Title: 푸앙이와 종윤이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.cc
+++ 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.cc
@@ -1,12 +1,24 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+ 
+int main(){
+    int a,b,c,d,q,r;
+    int x,y;
+    cin>>x>>y;
+    a = 100-x;
+    b = 100-y;
+    c = 100-(a+b);
+    d = a*b;
+    q = d/100;
+    r = d%100;
+    cout<<a<<' '<<b<<' '<<c<<' '<<d<<' '<<q<<' '<<r<<'\n';
+    cout<<c+q<<' '<<r;
+}
