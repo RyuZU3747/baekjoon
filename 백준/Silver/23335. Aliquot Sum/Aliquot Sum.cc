d3b40776624eef26373dc057e82e30232eb6e4f6
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:01 +0900

    [Silver V] Title: Aliquot Sum, Time: 6612 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/23335. Aliquot Sum/Aliquot Sum.cc
+++ 백준/Silver/23335. Aliquot Sum/Aliquot Sum.cc
@@ -1,12 +1,58 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+
+ll state[1000001];
+ll t,n;
+
+ll getSum(ll n){
+	ll ret = 0;
+	if(n==1) return 0;
+	ret+=1;
+	for(ll i=2;i*i<=n;i++){
+		if(n%i==0){
+			if(i*i==n) ret+=i;
+			else ret+=(i+n/i);
+		}
+	}
+	return ret;
+}
+
+void getState(ll n){
+	ll sum = getSum(n);
+	if(sum>n)
+		state[n]=1;
+	else if(sum<n)
+		state[n]=2;
+	else state[n]=3;
+}
+
+int main(){
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin>>t;
+	while(t--){
+		cin>>n;
+		if(state[n]==0)
+			getState(n);
+
+		switch(state[n]){
+			case 1:
+				cout<<"abundant"<<'\n';
+				break;
+			case 2:
+				cout<<"deficient"<<'\n';
+				break;
+			case 3:
+				cout<<"perfect"<<'\n';
+				break;
+		}
+	}
+}
