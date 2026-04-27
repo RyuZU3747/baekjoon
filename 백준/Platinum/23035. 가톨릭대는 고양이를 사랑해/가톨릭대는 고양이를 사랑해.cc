e383a379f64d572b9265fca9ef310c1aa7d703b6
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:21:30 +0900

    [Platinum IV] Title: 가톨릭대는 고양이를 사랑해, Time: 128 ms, Memory: 5764 KB -BaekjoonHub

--- 백준/Platinum/23035. 가톨릭대는 고양이를 사랑해/가톨릭대는 고양이를 사랑해.cc
+++ 백준/Platinum/23035. 가톨릭대는 고양이를 사랑해/가톨릭대는 고양이를 사랑해.cc
@@ -1,12 +1,27 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define MOD 998244353
+#define pii pair<ll,ll>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    ll n,m;cin>>n>>m;
+    ll t;cin>>t;
+    vector<pii> cats;
+    for(ll i=0;i<t;i++){
+        ll a,b;cin>>a>>b;
+        if(a<=n&&b<=m) cats.push_back({a,b});
+    }
+    sort(cats.begin(),cats.end());
+    ll lis[100001];
+    ll back = 1;
+    memset(lis, -1, sizeof lis);
+    lis[0] = cats[0].second;
+    for(ll i=1;i<cats.size();i++){
+        if(lis[back-1] <= cats[i].second) lis[back++] = cats[i].second;
+        else lis[upper_bound(lis, lis+back, cats[i].second) - lis] = cats[i].second;
+    }
+    cout<<back;
+}
