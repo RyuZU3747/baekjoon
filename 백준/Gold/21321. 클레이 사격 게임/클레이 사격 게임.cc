08d9574d2c4884fe1df52b2e3a19b07ecb03b125
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:58:53 +0900

    [Gold I] Title: 클레이 사격 게임, Time: 20 ms, Memory: 2532 KB -BaekjoonHub

--- 백준/Gold/21321. 클레이 사격 게임/클레이 사격 게임.cc
+++ 백준/Gold/21321. 클레이 사격 게임/클레이 사격 게임.cc
@@ -1,12 +1,38 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+#define PI 3.1415926535
+using namespace std;
+
+int dp[1<<17];
+int n;
+vector<pii> clay;
+
+int recur(int status, int round){
+    int & ret = dp[status];
+    if(ret!=0) return ret;
+
+    for(int i=0;i<n;i++){
+        if(status & (1<<i)) continue;
+        int no = 0;
+        for(int j=0;j<i;j++){
+            if((status & (1<<j))==0&&clay[i].first % clay[j].first == 0) no = 1;
+        }
+        if(no) continue;
+        ret = max(ret, recur(status ^ (1<<i), round+1)+round * clay[i].second);
+    }
+    return ret;
+}
+
+int main(){
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a,b;cin>>a>>b;
+        clay.push_back({a, b});
+    }
+    cout<<recur(0,1);
+}
