8566c696b167bd81da336c63dd9c95666d5c82fe
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:59:36 +0900

    [Gold IV] Title: 어떤 우유의 배달목록 (Easy), Time: 8 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/23835. 어떤 우유의 배달목록 （Easy）/어떤 우유의 배달목록 （Easy）.cc
+++ 백준/Gold/23835. 어떤 우유의 배달목록 （Easy）/어떤 우유의 배달목록 （Easy）.cc
@@ -1,12 +1,50 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define MOD 998244353
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+vector<int> graph[1001];
+vector<pii> route;
+int cost[1001];
+
+void dfs(int cur, int ed, int bef, int su){
+    if(cur==ed){
+        for(auto e: route){
+            cost[e.first] += e.second;
+        }
+        cost[cur] += su;
+        return;
+    }
+    for(auto nxt: graph[cur]){
+        if(nxt==bef) continue;
+        route.push_back({cur, su});
+        dfs(nxt, ed, cur, su+1);
+        route.pop_back();
+    }
+}
+
+int main(){
+    FASTIO;
+    int n;cin>>n;
+    for(int i=0;i<n-1;i++){
+        int a,b;cin>>a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    int q;cin>>q;
+    while(q--){
+        int a;cin>>a;
+        if(a==1){
+            int b,c;cin>>b>>c;
+            dfs(b,c,0,0);
+        }
+        else{
+            int b;cin>>b;
+            cout<<cost[b]<<'\n';
+        }
+    }
+}
