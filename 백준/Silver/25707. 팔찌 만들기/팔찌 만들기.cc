3d1baf48b543822243334e64cc40b26f2a69c572
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:50 +0900

    [Silver V] Title: 팔찌 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/25707. 팔찌 만들기/팔찌 만들기.cc
+++ 백준/Silver/25707. 팔찌 만들기/팔찌 만들기.cc
@@ -1,12 +1,26 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+
+int main(){
+    int n;
+    vector<int> arr;
+    int mn = 1234567890;
+    int mx = -1;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int tmp;cin>>tmp;
+        arr.push_back(tmp);
+        mn = min(mn,tmp);
+        mx = max(mx,tmp);
+    }
+    cout<<2*(mx-mn);
+}
