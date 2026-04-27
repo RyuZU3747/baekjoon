3830cfb2411e83dd22af199e94285df4a5e311d0
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:24:48 +0900

    [Silver V] Title: 멘토와 멘티, Time: 388 ms, Memory: 22232 KB -BaekjoonHub

--- 백준/Silver/26265. 멘토와 멘티/멘토와 멘티.cc
+++ 백준/Silver/26265. 멘토와 멘티/멘토와 멘티.cc
@@ -1,12 +1,21 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    map<string,vector<string>> a;
+    for(int i=0;i<n;i++){
+        string x,y;cin>>x>>y;
+        a[x].push_back(y);
+    }
+    for(auto x: a){
+        sort(x.second.begin(),x.second.end());
+        for(int i=x.second.size()-1;i>=0;i--) cout<<x.first<<' '<<x.second[i]<<'\n';
+    }
+}
