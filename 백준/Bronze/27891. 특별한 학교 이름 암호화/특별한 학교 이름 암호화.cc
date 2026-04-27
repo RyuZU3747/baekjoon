ae8789652858201ab98b986fa16987b09ef169e8
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:57:12 +0900

    [Bronze II] Title: 특별한 학교 이름 암호화, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/27891. 특별한 학교 이름 암호화/특별한 학교 이름 암호화.cc
+++ 백준/Bronze/27891. 특별한 학교 이름 암호화/특별한 학교 이름 암호화.cc
@@ -1,12 +1,16 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    string s;cin>>s;
+    if(s[3]==s[8]) cout<<"KIS";
+    else if(s[0]==s[6]) cout<<"SJA";
+    else if(s[1]==s[6]) cout<<"NLCS";
+    else cout<<"BHA";
+}
