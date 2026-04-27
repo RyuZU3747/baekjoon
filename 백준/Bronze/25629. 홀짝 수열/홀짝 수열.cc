c1d81f317f6b5144a55bc1270903aa4398b97b80
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:53:42 +0900

    [Bronze III] Title: 홀짝 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25629. 홀짝 수열/홀짝 수열.cc
+++ 백준/Bronze/25629. 홀짝 수열/홀짝 수열.cc
@@ -1,12 +1,31 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+ 
+int main(){
+    int n;
+    int arr[101];
+    cin>>n;
+    int odd = 0;
+    int even = 0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        if(arr[i]%2) odd++;
+        else even++;
+    }
+    if(n%2){
+        if(odd-1!=even) cout<<0;
+        else cout<<1;
+    }
+    else{
+        if(odd==even) cout<<1;
+        else cout<<0;
+    }
+}
