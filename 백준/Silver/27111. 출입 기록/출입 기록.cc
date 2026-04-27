e137e5567c12c3db4d580820676f9b80eeb40bff
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:25:00 +0900

    [Silver V] Title: 출입 기록, Time: 236 ms, Memory: 11396 KB -BaekjoonHub

--- 백준/Silver/27111. 출입 기록/출입 기록.cc
+++ 백준/Silver/27111. 출입 기록/출입 기록.cc
@@ -1,12 +1,30 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    map<int,int> l;
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        int a, b;
+        cin>>a>>b;
+        if(b==0){
+            if(l[a]==0) ans++;
+            l[a] = b;
+        }
+        else{
+            if(l[a]==1) ans++;
+            l[a] = b;
+        }
+    }
+    for(int i=0;i<200001;i++){
+        if(l[i]==1) ans++;
+    }
+    cout<<ans;
+}
