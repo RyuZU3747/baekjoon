6e9509e4747f23cff4f5af02dd9f4cec77dbe490
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:21:13 +0900

    [Gold III] Title: 자석, Time: 76 ms, Memory: 8104 KB -BaekjoonHub

--- 백준/Gold/28303. 자석/자석.cc
+++ 백준/Gold/28303. 자석/자석.cc
@@ -1,12 +1,36 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int n,k;cin>>n>>k;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int a;cin>>a;
+        arr.push_back(a);
+    }
+    vector<int> minarr(n);
+    vector<int> minarr2(n);
+    minarr[0] = arr[0] - 0;
+    minarr2[n-1] = arr[n-1] + k*(n-1);
+    for(int i=1;i<n;i++){
+        minarr[i] = min(minarr[i-1], arr[i] - k*i);
+    }
+    for(int i=n-2;i>=0;i--){
+        minarr2[i] = min(minarr2[i+1], arr[i] + k*i);
+    }
+    int ans = -1234567890;
+    for(int i=1;i<n;i++){
+        ans = max(ans, arr[i] - k*i - minarr[i-1]);
+    }
+    for(int i=n-2;i>=0;i--){
+        ans = max(ans, arr[i] + k*i - minarr2[i+1]);
+    }
+    cout<<ans;
+}
