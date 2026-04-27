bb07a4ab976418980c80da878e1a2438ac1af22e
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:52:56 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -1,12 +1,35 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+int cnt;
+
+int recursion(const char *s, int l, int r){
+    cnt++;
+    if(l >= r) return 1;
+    else if(s[l] != s[r]) return 0;
+    else return recursion(s, l+1, r-1);
+}
+
+int isPalindrome(const char *s){
+    return recursion(s, 0, strlen(s)-1);
+}
+
+int main(){
+    int t;
+    char s[1001];
+    cin>>t;
+    while(t--){
+        cnt = 0;
+        scanf(" %s",s);
+        int ans = isPalindrome(s);
+        cout<<ans<<' '<<cnt<<'\n';
+    }
+}
