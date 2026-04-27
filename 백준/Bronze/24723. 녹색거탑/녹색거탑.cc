3f8f6cbd2324215dc154c79db0a41aff093674fa
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:51:09 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -1,12 +1,19 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+
+/*------------------------------------------------------------*/
+
+int main(){
+    int a;
+    cin>>a;
+    int ans = 1;
+    for(int i=1;i<=a;i++) ans *= 2;
+    cout<<ans;
+}
