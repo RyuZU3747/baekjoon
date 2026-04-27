0149150d226e310291423455a56ce54d1a8f4d4c
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:52:41 +0900

    [Bronze I] Title: 인생은 한 방, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25183. 인생은 한 방/인생은 한 방.cc
+++ 백준/Bronze/25183. 인생은 한 방/인생은 한 방.cc
@@ -1,12 +1,26 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long int
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	int n;
+	char lotto[100001];
+	cin>>n;
+	cin>>lotto;
+	int cnt=1,flag = 0;
+	for(int i=1;i<n;i++){
+		
+			if(abs(lotto[i]-lotto[i-1])==1){
+				cnt++;
+				if(cnt>=5) flag = 1;
+			}
+			else cnt = 1;
+		
+	}
+	if(flag) cout<<"YES";
+	else cout<<"NO";
+}
