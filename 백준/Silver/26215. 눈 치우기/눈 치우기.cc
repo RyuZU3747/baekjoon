d263b6dd00bc0b7b08d546417b30aa64a116b8aa
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:24:44 +0900

    [Silver III] Title: 눈 치우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/26215. 눈 치우기/눈 치우기.cc
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.cc
@@ -1,12 +1,43 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    int arr[101];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int ans = 0;
+    int out = 1;
+    while(out){
+        int idx;
+        int mx = 0;
+        for(int i=0;i<n;i++){
+            if(arr[mx]<arr[i]) mx = i;
+        }
+        arr[mx]--;
+        for(int i=0;i<n;i++){
+            if(i==mx) continue;
+            if(arr[i]>0){
+                arr[i]--;
+                break;
+            }
+        }
+        ans++;
+        if(ans>=1441){
+            ans = -1;
+            break;
+        }
+        out = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>0) out = 1;
+        }
+    }
+    cout<<ans;
+}
