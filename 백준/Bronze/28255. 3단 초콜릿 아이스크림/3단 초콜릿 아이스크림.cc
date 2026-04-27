684f8adc8711969b8f42661d6a41664ac6799306
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:58:28 +0900

    [Bronze I] Title: 3단 초콜릿 아이스크림, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Bronze/28255. 3단 초콜릿 아이스크림/3단 초콜릿 아이스크림.cc
+++ 백준/Bronze/28255. 3단 초콜릿 아이스크림/3단 초콜릿 아이스크림.cc
@@ -1,12 +1,32 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+	FASTIO;
+	int t;cin>>t;
+	while(t--){
+		string s;cin>>s;
+		int len = s.length();
+		int par = ceil(len/3.0);
+		string ss;
+		string rev;
+		string tail;
+		string tarev;
+		for(int i=0;i<par;i++) ss += s[i];
+		for(int i=1;i<par;i++) tail += s[i];
+		for(int i=par-1;i>=0;i--) rev += s[i];
+		for(int i=1;i<par;i++) tarev += rev[i];
+		string t1 = ss + rev + ss;
+		string t2 = ss + rev + tail;
+		string t3 = ss + tarev + ss;
+		string t4 = ss + tarev + tail;
+		if(s==t1||s==t2||s==t3||s==t4) cout<<1<<'\n';
+		else cout<<0<<'\n';
+	}
+}
