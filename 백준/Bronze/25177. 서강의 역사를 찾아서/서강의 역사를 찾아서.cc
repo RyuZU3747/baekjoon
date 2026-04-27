4a39b05ca472182249087a67aef8c6076a858e04
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:52:32 +0900

    [Bronze II] Title: 서강의 역사를 찾아서, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25177. 서강의 역사를 찾아서/서강의 역사를 찾아서.cc
+++ 백준/Bronze/25177. 서강의 역사를 찾아서/서강의 역사를 찾아서.cc
@@ -1,12 +1,20 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long int
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	int n,m,cur[1001]={0,},past[1001]={0,};
+	cin>>n>>m;
+	for(int i=0;i<n;i++) cin>>cur[i];
+	for(int i=0;i<m;i++) cin>>past[i];
+	int len = Max(n,m);
+	int mx = 0;
+	for(int i=0;i<len;i++){
+		mx = Max(mx,past[i]-cur[i]);
+	}
+	cout<<mx;
+}
