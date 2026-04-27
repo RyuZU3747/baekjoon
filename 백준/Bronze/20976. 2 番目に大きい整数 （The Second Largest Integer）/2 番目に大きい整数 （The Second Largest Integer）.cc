d02854d05714013c68665dfe22505f57743cd061
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:47:38 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.cc
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.cc
@@ -1,12 +1,18 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+
+/*------------------------------------------------------------*/
+
+int main(){
+    vector<int> a(3);
+    cin>>a[0]>>a[1]>>a[2];
+    sort(a.begin(),a.end());
+    cout<<a[1];
+}
