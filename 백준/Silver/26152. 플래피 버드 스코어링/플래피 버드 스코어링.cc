332f74e13021097201baf0de8bd2d103fe75125e
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:24:35 +0900

    [Silver I] Title: 플래피 버드 스코어링, Time: 136 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/26152. 플래피 버드 스코어링/플래피 버드 스코어링.cc
+++ 백준/Silver/26152. 플래피 버드 스코어링/플래피 버드 스코어링.cc
@@ -1,12 +1,37 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int u[250001];
+int d[250001];
+int sz[250001];
+int minsz[250001];
+
+int main(){
+    FASTIO;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>u[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>d[i];
+        sz[i] = u[i] - d[i];
+    }//최소 사이즈의 배열에서 upper?
+    minsz[0] = sz[0];
+    for(int i=1;i<n;i++){
+        minsz[i] = min(minsz[i-1],sz[i]);
+    }
+    sort(minsz,minsz+n);
+    int q;cin>>q;
+    for(int i=0;i<q;i++){
+        int a;cin>>a;
+        int cnt = lower_bound(minsz,minsz+n,a)-minsz;
+        cout<<n-cnt<<'\n';
+    }
+}
