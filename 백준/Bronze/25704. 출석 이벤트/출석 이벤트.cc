31f85c2ce23b42e0d5c22206e6d18afc694ba43e
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:54:09 +0900

    [Bronze IV] Title: 출석 이벤트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25704. 출석 이벤트/출석 이벤트.cc
+++ 백준/Bronze/25704. 출석 이벤트/출석 이벤트.cc
@@ -1,12 +1,32 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+
+/*------------------------------------------------------------*/
+int main(){
+    int n;
+    int p;
+    cin>>n>>p;
+    int mn = p;
+    if(n>=5){
+        mn = min(mn,p-500);
+        if(n>=10){
+            mn = min(mn,p*9/10);
+            if(n>=15){
+                mn = min(mn,p-2000);
+                if(n>=20){
+                    mn = min(mn,p*75/100);
+                }
+            }
+        }
+    }
+    if(mn<=0) cout<<0;
+    else cout<<mn;
+}
