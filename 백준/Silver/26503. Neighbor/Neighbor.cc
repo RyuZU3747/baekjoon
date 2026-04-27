8a342cf2f207895a418ecf98154572d76a5bc66d
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:24:52 +0900

    [Silver V] Title: Neighbor, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/26503. Neighbor/Neighbor.cc
+++ 백준/Silver/26503. Neighbor/Neighbor.cc
@@ -1,12 +1,36 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    ll _;cin>>_;
+    while(_--){
+        ll au,ad,bu,bd;
+        cin>>au>>ad>>bu>>bd;
+
+        ll agcd = gcd(au,ad);
+        ll bgcd = gcd(bu,bd);
+        au /= agcd;
+        ad /= agcd;
+        bu /= bgcd;
+        bd /= bgcd;
+
+        ll g = gcd(bd,ad);
+        ll nd = lcm(ad,bd);
+        ll muta = bd/g;
+        ll mutb = ad/g;
+        ll nau = au * muta;
+        ll nbu = bu * mutb;
+        if(abs(nau - nbu) == 1){
+            cout<<"1/"<<nd<<'\n';
+        }
+        else{
+            cout<<"NOT NEIGHBORS\n";
+        }
+    }    
+}
