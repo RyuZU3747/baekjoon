65b8747ec9846c609b05279e0d48a04e9b3a58db
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:56:13 +0900

    [Bronze II] Title: 年齢の差 (Age Difference), Time: 64 ms, Memory: 3000 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27335. 年齢の差 （Age Difference）/年齢の差 （Age Difference）.cc
+++ 백준/Bronze/27335. 年齢の差 （Age Difference）/年齢の差 （Age Difference）.cc
@@ -1,12 +1,23 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define ll long long int
+#define MOD 1000000003
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int n;cin>>n;
+    vector<int> a(n);
+    for(auto &e: a) cin>>e;
+    int mn = *min_element(a.begin(), a.end());
+    int mx = *max_element(a.begin(), a.end());
+    for(auto e: a){
+        cout<<max(abs(e-mn), abs(e-mx))<<'\n';
+    }
+}
