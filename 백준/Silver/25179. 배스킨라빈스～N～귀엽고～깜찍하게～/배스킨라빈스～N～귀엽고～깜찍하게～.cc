e716bb79027beaea7ce6d0b7ce359bc20eb599cb
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:35 +0900

    [Silver IV] Title: 배스킨라빈스~N~귀엽고~깜찍하게~, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/25179. 배스킨라빈스～N～귀엽고～깜찍하게～/배스킨라빈스～N～귀엽고～깜찍하게～.cc
+++ 백준/Silver/25179. 배스킨라빈스～N～귀엽고～깜찍하게～/배스킨라빈스～N～귀엽고～깜찍하게～.cc
@@ -1,12 +1,14 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long int
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+	ll n,m;
+	cin>>n>>m;
+	if((n-1)%(m+1)!=0) cout<<"Can win";
+	else cout<<"Can't win";
+}
