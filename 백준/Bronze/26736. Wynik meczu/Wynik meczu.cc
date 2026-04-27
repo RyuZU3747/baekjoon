fd910cea96831f342a2d263bcd20f035aa1749ad
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:55:44 +0900

    [Bronze IV] Title: Wynik meczu, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Bronze/26736. Wynik meczu/Wynik meczu.cc
+++ 백준/Bronze/26736. Wynik meczu/Wynik meczu.cc
@@ -1,12 +1,19 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<ll,ll>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int a = 0, b = 0;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='A') a++;
+        else b++;
+    }
+    cout<<a<<" : "<<b;
+}
