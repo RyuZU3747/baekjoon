90e195f660902905e7f622e12d62bd4c35074c8d
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:19:24 +0900

    [Gold IV] Title: 곰곰아 선 넘지마, Time: 12 ms, Memory: 5024 KB -BaekjoonHub

--- 백준/Gold/26075. 곰곰아 선 넘지마/곰곰아 선 넘지마.cc
+++ 백준/Gold/26075. 곰곰아 선 넘지마/곰곰아 선 넘지마.cc
@@ -1,12 +1,34 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n,m;cin>>n>>m;
+    string s,t;cin>>s>>t;
+    vector<ll> sidx;
+    vector<ll> tidx;
+    for(int i=0;i<n+m;i++){
+        if(s[i]=='0') sidx.push_back(i);
+        if(t[i]=='0') tidx.push_back(i);
+    }
+    ll difs = 0;
+    for(int i=0;i<n;i++){
+        difs += abs(sidx[i] - tidx[i]);
+    }
+    ll ans;
+    if(difs%2==0){
+        ans = difs/2;
+        cout<<ans*ans*2;
+    }
+    else{
+        ans = difs/2;
+        ans *= ans;
+        ans += (difs/2+1)*(difs/2+1);
+        cout<<ans;
+    }
+}
