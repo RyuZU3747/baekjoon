d34b9cb49f4957c49c6bddb788c485e771e71538
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:53:55 +0900

    [Bronze III] Title: 균형 잡힌 소떡소떡, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/25641. 균형 잡힌 소떡소떡/균형 잡힌 소떡소떡.cc
+++ 백준/Bronze/25641. 균형 잡힌 소떡소떡/균형 잡힌 소떡소떡.cc
@@ -1,12 +1,31 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+ 
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int cnts = 0;
+    int cntt = 0;
+    for(int i=0;i<n;i++){
+        if(s[i]=='s') cnts++;
+        else cntt++;
+    }
+    for(int i=0;i<n;i++){
+        if(cnts==cntt){
+            cout<<&s[i];
+            return 0;
+        }
+        if(s[i]=='s') cnts--;
+        else cntt--;
+    }
+}
