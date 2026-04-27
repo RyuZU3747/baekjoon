dae493266af3d133ca186f45c4ea5d3467a176a6
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:51:24 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24736. Football Scoring/Football Scoring.cc
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -1,12 +1,21 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+
+/*------------------------------------------------------------*/
+
+int main(){
+    int a[2][5];
+    for(int i=0;i<5;i++) cin>>a[0][i];
+    for(int i=0;i<5;i++) cin>>a[1][i];
+    int ans[2];
+    ans[0] = a[0][0]*6+a[0][1]*3+a[0][2]*2+a[0][3]+a[0][4]*2;
+    ans[1] = a[1][0]*6+a[1][1]*3+a[1][2]*2+a[1][3]+a[1][4]*2;
+    cout<<ans[0]<<' '<<ans[1];
+}
