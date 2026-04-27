edb2a01c3e6f0e0befb39e062d1519489d858723
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:56:45 +0900

    [Bronze I] Title: 2023년은 검은 토끼의 해, Time: 256 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27494. 2023년은 검은 토끼의 해/2023년은 검은 토끼의 해.cc
+++ 백준/Bronze/27494. 2023년은 검은 토끼의 해/2023년은 검은 토끼의 해.cc
@@ -1,12 +1,30 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int ok(int n){
+    string s = "2023";
+    int idx = 0;
+    string num = to_string(n);
+    for(int i=0;i<num.length();i++){
+        if(num[i]==s[idx]){
+            idx++;
+        }
+    }
+    if(idx==4) return 1;
+    else return 0;
+}
+
+int main(){
+    int n;cin>>n;
+    int ans = 0;
+    for(int i=1;i<=n;i++){
+        if(ok(i)) ans++;
+    }
+    cout<<ans;
+}
