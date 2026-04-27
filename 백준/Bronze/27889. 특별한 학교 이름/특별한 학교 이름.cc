f88649ea69afb1ae1952c40e0b77a1d500e79e97
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:57:03 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -1,12 +1,16 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+	string s;cin>>s;
+	if(s.compare("NLCS")==0) cout<<"North London Collegiate School";
+	if(s.compare("BHA")==0) cout<<"Branksome Hall Asia";
+	if(s.compare("KIS")==0) cout<<"Korea International School";
+	if(s.compare("SJA")==0) cout<<"St. Johnsbury Academy";
+}
