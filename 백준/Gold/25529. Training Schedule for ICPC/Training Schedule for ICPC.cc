b68a82c1bc29b97feb061ab5f0d911b0ee584bba
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:17:13 +0900

    [Gold III] Title: Training Schedule for ICPC, Time: 124 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/25529. Training Schedule for ICPC/Training Schedule for ICPC.cc
+++ 백준/Gold/25529. Training Schedule for ICPC/Training Schedule for ICPC.cc
@@ -1,12 +1,35 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define ll long long int
+#define MOD 1000000007
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    while(1){
+        ll n,m;cin>>n>>m;
+        if(n==0&&m==0) break;
+        if(n==0){
+            cout<<-m*m<<'\n';
+            continue;
+        }
+        if(m==0){
+            cout<<n*n<<'\n';
+            continue;
+        }
+        ll ans = -1234567890123456789;
+
+        for(int sz=1;sz<=n && sz <= m;sz++){
+            ll plu = (n-sz+1)*(n-sz+1) + sz - 1;
+            ll minu = (m/(sz+1) + 1)*(m/(sz+1) + 1) * (m%(sz+1)) + (m/(sz+1))*(m/(sz+1)) * (sz+1-(m%(sz+1)));
+            ans = max(ans, plu-minu);
+        }
+        cout<<ans<<'\n';
+    }
+}
