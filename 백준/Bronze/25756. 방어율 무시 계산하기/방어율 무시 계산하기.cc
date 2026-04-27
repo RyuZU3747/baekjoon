8a84f10b7bc49770361c01860471a1118dc9d241
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:54:24 +0900

    [Bronze III] Title: 방어율 무시 계산하기, Time: 40 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Bronze/25756. 방어율 무시 계산하기/방어율 무시 계산하기.cc
+++ 백준/Bronze/25756. 방어율 무시 계산하기/방어율 무시 계산하기.cc
@@ -1,12 +1,28 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+
+int main(){
+    int n;
+    cin>>n;
+    double arr[40001];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    double ans = arr[0];
+    cout.precision(6);
+    cout<<fixed;
+    cout<<ans<<'\n';
+    for(int i=1;i<n;i++){
+        ans = (1-(1-(ans/100.0))*(1-(arr[i]/100.0)))*100;
+        cout<<ans<<'\n';
+    }
+}
