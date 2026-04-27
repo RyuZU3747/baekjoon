3f80146326153fb1278204f84bf6488e7ddf8b9e
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:28:34 +0900

    [Silver III] Title: 고양이 카페, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/28353. 고양이 카페/고양이 카페.cc
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.cc
@@ -1,12 +1,29 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n,k;cin>>n>>k;
+    vector<int> a(n);
+    for(auto &x:a) cin>>x;
+    sort(a.begin(),a.end());
+    int ans = 0;
+    int left = 0, right = n-1;
+    while(left<right){
+        if(a[right] >= k) right--;
+        if(a[left] + a[right] <= k){
+            ans++;
+            left++;
+            right--;
+        }
+        else{
+            right--;
+        }
+    }
+    cout<<ans;
+}
