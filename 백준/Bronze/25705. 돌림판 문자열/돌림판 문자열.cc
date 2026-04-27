3c496a5b0ee77759d6bb3b820399d1b5ff4c5f32
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:54:14 +0900

    [Bronze I] Title: 돌림판 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/25705. 돌림판 문자열/돌림판 문자열.cc
+++ 백준/Bronze/25705. 돌림판 문자열/돌림판 문자열.cc
@@ -1,12 +1,37 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+
+int main(){
+    int n,m;
+    string dol, s;
+    cin>>n;
+    cin>>dol;
+    cin>>m;
+    cin>>s;
+    int cnt = 0;
+    int dolp = dol.length()-1;
+    int mod = dol.length();
+    for(int i=0;i<m;i++){
+        for(int k=0;k<n;k++){
+            dolp++;
+            dolp %= mod;
+            cnt++;
+            if(s[i]==dol[dolp]) break;
+        }
+        if(s[i]!=dol[dolp]){
+            cout<<"-1";
+            return 0;
+        }
+
+    }
+    cout<<cnt;
+}
