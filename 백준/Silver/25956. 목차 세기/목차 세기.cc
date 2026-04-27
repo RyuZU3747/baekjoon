a49bf3e1bd02db5978e120d860e3aaa36bb921c3
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:24:15 +0900

    [Silver I] Title: 목차 세기, Time: 340 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/25956. 목차 세기/목차 세기.cc
+++ 백준/Silver/25956. 목차 세기/목차 세기.cc
@@ -1,12 +1,46 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define pii pair<int,int>
+using namespace std;
+
+int ar[1000001];
+int ans[1000001];
+
+int main(){
+    int n;cin>>n;
+    stack<pii> st;//idx, head
+    pii parent;
+    for(int i=0;i<n;i++){
+        cin>>ar[i];
+        if(ar[i]!=1&&st.empty()){
+            cout<<-1;
+            return 0;
+        }
+        if(ar[i]==1){
+            st.push(make_pair(i,ar[i]));
+            parent = make_pair(i,ar[i]);
+        }
+        else{
+            if(st.top().second==ar[i]){
+                ans[parent.first]++;
+            }
+            else if(st.top().second==ar[i]-1){
+                parent = make_pair(st.top().first,st.top().second);
+                ans[parent.first]++;
+                st.push(make_pair(i,ar[i]));
+            }
+            else if(st.top().second<ar[i]-1){
+                cout<<-1;
+                return 0;
+            }
+            else if(st.top().second>ar[i]){
+                while(st.top().second!=ar[i]) st.pop();
+                if(st.top().second!=1){
+                    st.pop();
+                    ans[st.top().first]++;
+                    st.push(make_pair(i,ar[i]));
+                }
+            }
+        }
+    }
+    for(int i=0;i<n;i++) cout<<ans[i]<<'\n';
+}
