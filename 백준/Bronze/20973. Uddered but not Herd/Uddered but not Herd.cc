229f7a1188e36d5ad59bed530c921f79a1104d07
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:47:27 +0900

    [Bronze I] Title: Uddered but not Herd, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/20973. Uddered but not Herd/Uddered but not Herd.cc
+++ 백준/Bronze/20973. Uddered but not Herd/Uddered but not Herd.cc
@@ -1,12 +1,23 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    string cowp;
+    string humm;
+    cin>>cowp;
+    cin>>humm;
+    int i = 0, cnt = 0;
+    while(i<humm.length()){
+        for(int j=0;j<cowp.length();j++){
+            if(humm[i]==cowp[j]) i++;
+        }
+        cnt++;
+    }
+    cout<<cnt;
+}
