ea76eef1adc5bfebc3935382e8fd8553962af997
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:45 +0900

    [Silver III] Title: 자전거 묘기, Time: 100 ms, Memory: 3464 KB -BaekjoonHub

--- 백준/Silver/25706. 자전거 묘기/자전거 묘기.cc
+++ 백준/Silver/25706. 자전거 묘기/자전거 묘기.cc
@@ -1,12 +1,27 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+
+int main(){
+    int n;
+    int arr[200001];
+    int dp[200001];
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        dp[i]=1;
+    }
+    for(int i=n-2;i>=0;i--){
+        if(i+arr[i]+1<n) dp[i] = dp[i+arr[i]+1]+1;
+    }
+    for(int i=0;i<n;i++) cout<<dp[i]<<' ';
+
+}
