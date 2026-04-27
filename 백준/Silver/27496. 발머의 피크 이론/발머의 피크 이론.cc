8cdabd02a468fc429d78ef94eda0e24fa78d0a38
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:25:52 +0900

    [Silver III] Title: 발머의 피크 이론, Time: 268 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.cc
+++ 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.cc
@@ -1,12 +1,28 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int arr[1000001];
+
+int main(){
+    int n,l;cin>>n>>l;
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int len = 0;
+    int sum = 0;
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        len++;
+        if(len>l){
+            sum-=arr[i-l];
+            len--;
+        }
+        if(sum>=129&&sum<=138) ans++;
+    }
+    cout<<ans;
+}
