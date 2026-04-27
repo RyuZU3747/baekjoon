e498a763b29010f2dcf7536e1650fc7b4d0497db
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:50:08 +0900

    [Bronze V] Title: 余り (Remainder), Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.py
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.py
@@ -1,12 +1,2 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+n = int(input())
+print(n%21)
