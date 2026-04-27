bce0082225fc76e1fb14cb510730fce101b8c07a
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:56:53 +0900

    [Bronze III] Title: 찬반투표, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27736. 찬반투표/찬반투표.cc
+++ 백준/Bronze/27736. 찬반투표/찬반투표.cc
@@ -1,12 +1,23 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    int sum = 0;
+    int su = 0;
+    for(int i=0;i<n;i++){
+        int a;cin>>a;
+        if(a==0) su++;
+        sum += a;
+    }
+    if(n%2&&su>=n/2+1) cout<<"INVALID";
+    else if(!(n%2)&&su>=n/2) cout<<"INVALID";
+    else if(sum>0) cout<<"APPROVED";
+    else cout<<"REJECTED";
+}
