4a9bc587f79f47860406dceaebc82a9e7627e37e
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:20:14 +0900

    [Gold IV] Title: 지수를 더하자, Time: 100 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Gold/27725. 지수를 더하자/지수를 더하자.cc
+++ 백준/Gold/27725. 지수를 더하자/지수를 더하자.cc
@@ -1,12 +1,24 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    int sosu[200001];
+    for(int i=0;i<n;i++) cin>>sosu[i];
+    ll k;cin>>k;
+    ll ans = 0;
+    for(int i=0;i<n;i++){
+        ll tmp = 1;
+        while(tmp<=k){
+            tmp *= sosu[i];
+            ans += floor(1.0*k/tmp);
+        }
+    }
+    cout<<ans;
+}
