ed4355d72994446180c19e2285aa4b85d6121624
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:27:12 +0900

    [Silver V] Title: 가지 산사태, Time: 764 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27940. 가지 산사태/가지 산사태.cc
+++ 백준/Silver/27940. 가지 산사태/가지 산사태.cc
@@ -1,12 +1,22 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n,m,k;cin>>n>>m>>k;
+    int flr[100001] = {0,};
+    for(int i=0;i<m;i++){
+        int t,r;cin>>t>>r;
+        flr[0] += r;
+        if(flr[0]>k){
+            cout<<i+1<<' '<<1;
+            return 0;
+        }
+    }
+    cout<<-1;
+}
