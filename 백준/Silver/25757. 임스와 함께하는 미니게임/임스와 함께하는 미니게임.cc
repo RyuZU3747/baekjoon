c2a88fc9c712726b5bb86409f71bd8e10c57c810
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:54 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 128 ms, Memory: 9948 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.cc
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.cc
@@ -1,12 +1,36 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+
+int main(){
+    set<string> s;
+    int n;
+    char k;
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        string tmp;
+        cin>>tmp;
+        s.insert(tmp);
+    }
+    int su = 0;
+    for(auto i: s){
+        su++;
+    }
+    if(k=='Y'){
+        cout<<su;
+    }
+    else if(k=='F'){
+        cout<<su/2;
+    }
+    else{
+        cout<<su/3;
+    }
+}
