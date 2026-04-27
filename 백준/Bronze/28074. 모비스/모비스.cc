afd366b19f683ac3003c0bc16b07fe8f47432f30
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:57:52 +0900

    [Bronze IV] Title: 모비스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/28074. 모비스/모비스.cc
+++ 백준/Bronze/28074. 모비스/모비스.cc
@@ -1,12 +1,27 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    string s;cin>>s;
+    int cnt[6] = {0,};
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='M') cnt[0]++;
+        if(s[i]=='O') cnt[1]++;
+        if(s[i]=='B') cnt[2]++;
+        if(s[i]=='I') cnt[3]++;
+        if(s[i]=='S') cnt[4]++;
+    }
+    for(int i=0;i<5;i++){
+        if(cnt[i]==0){
+            cout<<"NO";
+            return 0;
+        }
+    }
+    cout<<"YES";
+}
