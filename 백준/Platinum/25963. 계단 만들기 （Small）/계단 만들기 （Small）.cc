00be72a64298417c8d62ab1f780f2e31884f1435
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:22:07 +0900

    [Platinum V] Title: 계단 만들기 (Small), Time: 176 ms, Memory: 203252 KB -BaekjoonHub

--- 백준/Platinum/25963. 계단 만들기 （Small）/계단 만들기 （Small）.cc
+++ 백준/Platinum/25963. 계단 만들기 （Small）/계단 만들기 （Small）.cc
@@ -1,12 +1,46 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+
+int arr[101];
+int block;
+int n;
+
+int dp[101][5050][101];
+
+int solve(int idx, int bl, int cur){
+    int & ret = dp[idx][bl][cur];
+    if(ret!=-1) return ret;
+    if(idx==n){
+        if(bl==0) return 0;
+        else return 123456789;
+    }
+    if(bl<0) return 123456789;
+    if(cur>100||cur<0) return 123456789;
+    ret = min(solve(idx+1, bl-cur, cur+1), 
+        min(solve(idx+1, bl-cur, cur), 
+        solve(idx+1, bl-cur, cur-1))) + abs(arr[idx] - cur); 
+    return ret;
+}
+
+int main(){
+    cin>>n;
+    int ans = 123456789;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        block += arr[i];
+    }
+        memset(dp,-1,sizeof dp);
+    for(int i=0;i<101;i++){
+        ans = min(ans,solve(0,block,i));
+    }
+    if(ans==-1) ans = 0;
+    cout<<ans/2;
+}
