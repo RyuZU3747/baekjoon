56ab3e69d7043a40206098dfb2da9cb639c47a15
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:40 +0900

    [Silver V] Title: 아주 간단한 문제, Time: 172 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.py
+++ 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.py
@@ -1,12 +1,7 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys
+input = sys.stdin.readline
+
+q = int(input())
+for _ in range(q):
+    a, b = map(int, input().split())
+    print(1 if a < b and b % a == 0 else 0)
