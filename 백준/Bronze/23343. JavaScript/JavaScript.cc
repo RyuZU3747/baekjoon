4dcc6e04d21f4ac3ae28df1eddb5f5d0fa759da7
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:49:25 +0900

    [Bronze III] Title: JavaScript, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/23343. JavaScript/JavaScript.cc
+++ 백준/Bronze/23343. JavaScript/JavaScript.cc
@@ -1,12 +1,26 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+using namespace std;
+//cin.tie(0);
+//ios_base::sync_with_stdio(0);
+/*------------------------------------------------------------*/
+
+int main(){
+	string a, b;
+	cin>>a>>b;
+	int ta = strtol(&a[0],NULL,10);
+	int tb = strtol(&b[0],NULL,10);
+	if(a!=to_string(ta)){
+		cout<<"NaN";
+		return 0;
+	}
+	if(b!=to_string(tb)){
+		cout<<"NaN";
+		return 0;
+	}
+	cout<<ta-tb;
+}
