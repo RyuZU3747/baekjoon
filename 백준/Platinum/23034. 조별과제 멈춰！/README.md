a66853fff6d2303bfd9188b06d2d69219573f021
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:21:22 +0900

    [Platinum V] Title: 조별과제 멈춰!, Time: 60 ms, Memory: 8476 KB -BaekjoonHub

--- 백준/Platinum/23034. 조별과제 멈춰！/README.md
+++ 백준/Platinum/23034. 조별과제 멈춰！/README.md
@@ -12,11 +12,11 @@
 
 ### 제출 일자
 
-2026년 04월 28일 02:30:48
+2023년 10월 11일 19:30:45
 
 ### 문제 설명
 
-<p style="text-align: center;"><img alt="" src="" style="height: 500px; width: 500px;"></p>
+<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/2949c980-da11-4575-8cd9-39b0372e783e/-/preview/" style="height: 500px; width: 500px;"></p>
 
 <p>교수님이 시험 기간에 조별 과제를 준비하셨다...! 가톨릭대학교의 조교 아리는 <em>N</em>명의 학생을 2개의 조로 구성하여 과제 공지를 하려 한다. 이때, 구성된 각 조의 인원은 1명 이상이어야 한다. 각 학생은 1~<em>N</em>의 정수 중 고유한 번호를 학번으로 갖는다.</p>
 
--- 백준/Platinum/23034. 조별과제 멈춰！/조별과제 멈춰！.cc
+++ 백준/Platinum/23034. 조별과제 멈춰！/조별과제 멈춰！.cc
@@ -1,12 +1,74 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define ll long long int
+#define MOD 1000000007
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int root[1010];
+int mxedge[1010][1010];
+vector<pii> graph[1010];
+vector<array<int,3>> edges;
+
+int find(int x){
+    return root[x] = root[x] == x ? x : find(root[x]);
+}
+
+void Union(int x, int y){
+    int fx = find(x);
+    int fy = find(y);
+    if(fx>fy) root[fx]=root[fy];
+    else root[fy]=root[fx];
+    return;
+}
+
+int main(){
+    FASTIO;
+    for(int i=0;i<1010;i++){
+        root[i]=i;
+    }
+    int n,m;cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int a,b,c;cin>>a>>b>>c;
+        edges.push_back({c,a,b});
+    }
+    sort(edges.begin(), edges.end());
+    ll cost = 0;
+    for(auto [w,a,b] : edges){
+        if(find(a) != find(b)){
+            Union(a,b);
+            cost += w;
+            graph[a].push_back({b,w});
+            graph[b].push_back({a,w});
+        }
+    }
+
+    for(int i=1;i<=n;i++){
+        int vis[1010];
+        memset(vis, 0, sizeof vis);
+        queue<pii> q;
+        q.push({i,0});
+        vis[i] = 1;
+        while(!q.empty()){
+            auto [cur, cw] = q.front();
+            q.pop();
+            for(auto [nxt,nw]: graph[cur]){
+                if(vis[nxt]) continue;
+                vis[nxt] = 1;
+                mxedge[i][nxt] = max(cw, nw);
+                mxedge[nxt][i] = max(cw, nw);
+                q.push({nxt, mxedge[i][nxt]});
+            }
+        }
+    }
+    int q;cin>>q;
+    for(int i=0;i<q;i++){
+        int a,b;cin>>a>>b;
+        cout<<cost-mxedge[a][b]<<'\n';
+    }
+}
