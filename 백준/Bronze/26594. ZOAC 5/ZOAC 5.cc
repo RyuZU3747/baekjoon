6b4b7eb533bcff21d81e0e8409dcecc16a89e839
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:55:35 +0900

    [Bronze III] Title: ZOAC 5, Time: 36 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/26594. ZOAC 5/ZOAC 5.cc
+++ 백준/Bronze/26594. ZOAC 5/ZOAC 5.cc
@@ -1,12 +1,19 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<ll,ll>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    string a;
+    cin>>a;
+    int cnt = 1;
+    for(int i=1;i<a.length();i++){
+        if(a[i]==a[0]) cnt++;
+        else break;
+    }
+    cout<<cnt;
+}
