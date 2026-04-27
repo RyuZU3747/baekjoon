4637cadd3f76dd7a9b89ce9ba8fcc94e81357432
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:21:51 +0900

    [Platinum V] Title: Dr. Bill Poucher, Time: 312 ms, Memory: 19600 KB -BaekjoonHub

--- 백준/Platinum/23706. Dr． Bill Poucher/Dr． Bill Poucher.cc
+++ 백준/Platinum/23706. Dr． Bill Poucher/Dr． Bill Poucher.cc
@@ -1,12 +1,39 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <iostream>
+#include <vector>
+#include <queue>
+#define ll long long int
+#define MOD 1000000007
+using namespace std;
+
+vector<int> graph[300001];
+queue<int> q;
+int indegree[300001];
+
+
+int main(){
+    int n,m;cin>>n>>m;
+	for(int i=0;i<m;i++){
+        int x,y;cin>>x>>y;
+		graph[x].push_back(y);
+		indegree[y]++;
+	}
+	for(int i=0;i<n;i++){
+		if(indegree[i]==0){
+			q.push(i);
+		}
+	}
+    while(!q.empty()){
+		int cur = q.front();
+		q.pop();
+		for(int i=0;i<graph[cur].size();i++){
+			int next = graph[cur][i];
+			indegree[next]--;
+			if(indegree[next]==0) q.push(next);
+		}
+	}
+    int yes = 0;
+    for(int i=0;i<n;i++){
+        if(indegree[i] > 0) yes = 1;
+    }
+    cout<<yes;
+}
