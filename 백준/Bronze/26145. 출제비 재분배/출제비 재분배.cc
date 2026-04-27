29b33fd1014bbf3575ba85939b6917987b45ac67
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:54:56 +0900

    [Bronze III] Title: 출제비 재분배, Time: 452 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/26145. 출제비 재분배/출제비 재분배.cc
+++ 백준/Bronze/26145. 출제비 재분배/출제비 재분배.cc
@@ -1,12 +1,25 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n,m;cin>>n>>m;
+    int ar[2001];
+    memset(ar, 0, sizeof ar);
+    for(int i=0;i<n;i++) cin>>ar[i];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n+m;j++){
+            int tmp;cin>>tmp;
+            ar[j] += tmp;
+            ar[i] -= tmp;
+        }
+    }
+    for(int i=0;i<n+m;i++){
+        cout<<ar[i]<<' ';
+    }
+}
