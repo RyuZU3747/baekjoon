768792c7452c2b141717d1e602ca5ce18080c050
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:20:18 +0900

    [Gold V] Title: 연산자 파티, Time: 52 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/27738. 연산자 파티/연산자 파티.cc
+++ 백준/Gold/27738. 연산자 파티/연산자 파티.cc
@@ -1,12 +1,24 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    ll n;cin>>n;
+    ll x = 0;
+    ll a,b,c,d,e,f;cin>>a>>b>>c>>d>>e>>f;
+    ll st = n%f;
+    for(ll i=n-st+1;i<=n;i++){
+        if(i%a==0) x+=i;
+        if(i%b==0) x%=i;
+        if(i%c==0) x&=i;
+        if(i%d==0) x^=i;
+        if(i%e==0) x|=i;
+        if(i%f==0) x>>=i;
+    }
+    cout<<x;
+}
