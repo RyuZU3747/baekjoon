b954f5d0c87c533bfc2b08b7aec71a4f891417d0
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:28:45 +0900

    [Unrated] Title: $\textbf{multiple}\text{ edges}$, Time: 404 ms, Memory: 144760 KB -BaekjoonHub

--- 백준/Unrated/24903. $＼textbf｛multiple｝＼text｛ edges｝$/$＼textbf｛multiple｝＼text｛ edges｝$.cc
+++ 백준/Unrated/24903. $＼textbf｛multiple｝＼text｛ edges｝$/$＼textbf｛multiple｝＼text｛ edges｝$.cc
@@ -1,12 +1,16 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long
+#define INF 123456789
+using namespace std;
+
+int main(){
+	int n,q;
+	scanf("%d %d",&n,&q);
+	for(int i=0;i<q;i++){
+		if(i%2==0) printf("%d\n",n-1);
+		else printf("%d\n",n);
+	}
+}
