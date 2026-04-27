4ba85a74f6cb8c310aa6d7547e4bacda59f567f3
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:24:25 +0900

    [Silver II] Title: DPS, Time: 20 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/26084. DPS/DPS.cc
+++ 백준/Silver/26084. DPS/DPS.cc
@@ -1,12 +1,18 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <iostream>
+using namespace std;
+int su[27];
+int cnt[27];
+int main(){
+    string name;
+    std::cin>>name;
+    int n;std::cin>>n;
+    while(n--){
+        string tmp;
+        cin>>tmp;
+        su[tmp[0]-'A']++;
+    }
+    long long int ans = 1,div = 1;
+    for(int i=0;i<3;i++) ans *= su[name[i]-'A']--;
+    for(int i=0;i<3;i++) div *= ++cnt[name[i]-'A'];
+    std::cout<<ans/div;
+}
