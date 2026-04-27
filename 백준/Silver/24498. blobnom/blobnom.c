477d51b380df0ad1be39c8f38b0a77947c24c404
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:14 +0900

    [Silver III] Title: blobnom, Time: 232 ms, Memory: 8804 KB -BaekjoonHub

--- 백준/Silver/24498. blobnom/blobnom.c
+++ 백준/Silver/24498. blobnom/blobnom.c
@@ -1,12 +1,33 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long
+
+int main(){
+	ll int n,max=-1;
+	scanf("%lld",&n);
+	ll int a[1000001];
+	for(int i=0;i<n;i++){
+		scanf("%lld",&a[i]);
+	}
+	if(n==1) printf("%lld",a[0]);
+	else if(n==2) printf("%lld",Max(a[0],a[1]));
+	else{
+		for(int i=0;i<n;i++){
+			if(max<a[i]) max=a[i];
+			if(i>0&&i<n-1){
+				if(a[i-1]>a[i+1]){
+					if(max<a[i]+a[i+1]) max=a[i]+a[i+1];
+				}
+				else{
+					if(max<a[i]+a[i-1]) max=a[i]+a[i-1];
+				}
+			}
+		}
+		printf("%lld",max);
+	}
+}
