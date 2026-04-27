28187b3a2b89af0b70b5f7998083c43d121aea9b
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:22:42 +0900

    [Platinum V] Title: 즉흥 여행 (Easy), Time: 1532 ms, Memory: 67016 KB -BaekjoonHub

--- 백준/Platinum/26146. 즉흥 여행 （Easy）/즉흥 여행 （Easy）.cc
+++ 백준/Platinum/26146. 즉흥 여행 （Easy）/즉흥 여행 （Easy）.cc
@@ -1,12 +1,57 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+map<int,vector<int>> edge;
+map<int,vector<int>> redge;
+int vis[200001];
+stack<int> st;
+vector<vector<int>> scc(200001);
+int cnt;
+
+void dfs(int cur){
+    if(vis[cur]) return;
+    vis[cur] = 1;
+    for(auto e : edge[cur]){
+        dfs(e);
+    }
+    st.push(cur);
+}
+
+void dfs2(int cur){
+    if(vis[cur]) return;
+    vis[cur] = 1;
+    for(auto e: redge[cur]){
+        dfs2(e);
+    }
+    scc[cnt].push_back(cur);
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int a,b;cin>>a>>b;
+        edge[a].push_back(b);
+        redge[b].push_back(a);
+    }
+    for(int i=1;i<=n;i++){
+        if(vis[i]==1) continue;
+        dfs(i);
+    }
+    memset(vis, 0, sizeof vis);
+    while(!st.empty()){
+        int cur = st.top();
+        st.pop();
+        if(vis[cur]==1) continue;
+        dfs2(cur);
+        cnt++;
+    }
+    if(cnt==1) cout<<"Yes";
+    else cout<<"No";
+}
