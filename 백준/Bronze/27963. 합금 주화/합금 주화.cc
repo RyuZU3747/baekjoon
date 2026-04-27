8bf94bfd6f0427482295913c034c584cf74deb98
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:57:21 +0900

    [Bronze I] Title: 합금 주화, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27963. 합금 주화/합금 주화.cc
+++ 백준/Bronze/27963. 합금 주화/합금 주화.cc
@@ -1,12 +1,16 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    double d1,d2,x;cin>>d1>>d2>>x;
+    cout<<fixed;
+    cout.precision(8);
+    cout<<100.0/(x/max(d1,d2)+(100-x)/min(d1,d2));
+}
