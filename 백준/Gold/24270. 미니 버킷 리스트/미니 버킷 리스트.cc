2c0f5db6895b413f3c6ca302aec94970c2516c01
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:59:50 +0900

    [Gold IV] Title: 미니 버킷 리스트, Time: 64 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/24270. 미니 버킷 리스트/미니 버킷 리스트.cc
+++ 백준/Gold/24270. 미니 버킷 리스트/미니 버킷 리스트.cc
@@ -1,12 +1,23 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long
+#define div 1000000007
+
+int main(){
+	ll int n,k,s,ans=1;
+	scanf("%lld %lld",&n,&k);
+	for(int i=1;i<=n;i++){
+		scanf("%lld",&s);
+		k -= s;
+	}
+	
+	for(int i=k+1;i<=k+n;i++){
+		ans = (ans*i)%div;
+	}
+	printf("%lld",ans);
+}
