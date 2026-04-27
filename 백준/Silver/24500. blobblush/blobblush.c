adf84ec4a437e0c1b23bdf3af69474ba4643a411
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:23:27 +0900

    [Silver II] Title: blobblush, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/24500. blobblush/blobblush.c
+++ 백준/Silver/24500. blobblush/blobblush.c
@@ -1,12 +1,22 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#define Max(a,b) ((a)>(b) ? (a):(b))
+#define Min(a,b) ((a)<(b) ? (a):(b))
+#define swap(a,b) {int c; c=a;a=b;b=c;}
+#define ll long long int
+
+int main(){
+	ll n,tmp=0;
+	scanf("%lld",&n);
+	for(;(1LL<<tmp)<=n;tmp++);
+	tmp=1LL<<tmp;
+	if(n==tmp-1){
+		printf("1\n%lld",n);
+	}
+	else{
+		printf("2\n%lld %lld",n^(tmp-1),n);
+	}
+}
