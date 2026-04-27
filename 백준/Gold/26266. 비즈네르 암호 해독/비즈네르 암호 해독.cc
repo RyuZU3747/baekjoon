5f12256b1af38c6cdcb54041bc5fabdcb5e8174a
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:19:34 +0900

    [Gold V] Title: 비즈네르 암호 해독, Time: 24 ms, Memory: 3784 KB -BaekjoonHub

--- 백준/Gold/26266. 비즈네르 암호 해독/비즈네르 암호 해독.cc
+++ 백준/Gold/26266. 비즈네르 암호 해독/비즈네르 암호 해독.cc
@@ -1,12 +1,57 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+string arr;
+int fail[200200];
+
+void failfunc(string s){
+	int len = s.length(),j=0;
+	for(int i=1;i<len;i++){
+		while(j>0&&s[i]!=s[j]) j=fail[j-1];
+		if(s[i]==s[j]){
+			fail[i]=++j;
+		}
+	}
+}
+
+int main(){
+    string a, b;cin>>a>>b;
+    for(int i=0;i<a.length();i++){
+        arr.push_back(b[i]-'A');
+        arr[i] += 26;
+        arr[i] -= a[i]-'A';
+        arr[i] %= 26;
+    }
+    failfunc(arr);
+    int idx = fail[arr.length()-1];
+    if(arr.length()%(arr.length()-idx)!=0){
+        for(int i=0;i<arr.length();i++){
+            int tmp = arr[i]+'A'-1;
+            if(tmp<'A'){
+                tmp+=26;
+            }
+            if(tmp>'Z'){
+                tmp-=26;
+            }
+            cout<<(char)(tmp);
+        }
+    }
+    else{
+        for(int i=0;i<arr.length()-idx;i++){
+            int tmp = arr[i]+'A'-1;
+            if(tmp<'A'){
+                tmp+=26;
+            }
+            if(tmp>'Z'){
+                tmp-=26;
+            }
+            cout<<(char)(tmp);
+        }
+    }
+}
