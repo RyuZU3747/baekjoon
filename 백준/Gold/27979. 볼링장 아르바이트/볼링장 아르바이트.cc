a503c653beb1976d99fcec217147bebdc5fa4fbc
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:20:22 +0900

    [Gold IV] Title: 볼링장 아르바이트, Time: 100 ms, Memory: 3592 KB -BaekjoonHub

--- 백준/Gold/27979. 볼링장 아르바이트/볼링장 아르바이트.cc
+++ 백준/Gold/27979. 볼링장 아르바이트/볼링장 아르바이트.cc
@@ -1,12 +1,28 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define ll long long int
+#define MOD 1000000003
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    vector<int> arr(n);
+    for(auto &x:arr) cin>>x;
+    vector<int> dif = arr;
+    sort(arr.begin(),arr.end());
+    int ans = 0;
+    int idx = n-1;
+    for(int i=n-1;i>=0;i--){
+        if(dif[i]==arr[idx]){
+            ans++;
+            idx--;
+        }
+    }
+    cout<<n-ans;
+}
