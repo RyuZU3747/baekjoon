be106fe7eb4ff967189df14f7beaad4f631da822
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:56:38 +0900

    [Bronze II] Title: One and Two, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27487. One and Two/One and Two.cc
+++ 백준/Bronze/27487. One and Two/One and Two.cc
@@ -1,12 +1,40 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int _;cin>>_;
+    while(_--){
+        int n;cin>>n;
+        int arr[1011];
+        int left = 0;
+        int right = 0;
+        int cnt2 = 0;
+        for(int i=1;i<=n;i++){
+            cin>>arr[i];
+            if(arr[i]==2) cnt2++;
+        }
+        if(cnt2%2==1){
+            cout<<-1<<'\n';
+        }
+        else{
+            right = cnt2;
+            for(int i=1;i<n;i++){
+                if(arr[i]==2){
+                    left++;
+                    right--;
+                }
+                if(left==right){
+                    cout<<i<<'\n';
+                    break;
+                }
+            }
+        }
+    }
+}
