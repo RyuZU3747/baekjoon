7f61d74497cf5ec8bcf27d6d33dac7173e7fa558
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:27:48 +0900

    [Silver I] Title: 지연 평가, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27973. 지연 평가/지연 평가.cc
+++ 백준/Silver/27973. 지연 평가/지연 평가.cc
@@ -1,12 +1,32 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int q;cin>>q;
+    ll cur = 1;
+    ll mul = 1;
+    while(q--){
+        int n;cin>>n;
+        if(n==0){
+            int a;cin>>a;
+            cur += a;
+        }
+        if(n==1){
+            int a;cin>>a;
+            cur *= a;
+            mul *= a;
+        }
+        if(n==2){
+            int a;cin>>a;
+            cur += a*mul;
+        }
+        if(n==3) cout<<cur<<'\n';
+    }
+}
