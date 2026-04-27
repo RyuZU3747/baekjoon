f6db45f60c71b91be0adaec2cfb31a7f383f03d5
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:58:32 +0900

    [Bronze III] Title: 과 조사하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/28289. 과 조사하기/과 조사하기.cc
+++ 백준/Bronze/28289. 과 조사하기/과 조사하기.cc
@@ -1,12 +1,27 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    map<int,int> cnt;
+    cnt[0] = 0;
+    cnt[1] = 0;
+    cnt[2] = 0;
+    cnt[3] = 0;
+    for(int i=0;i<n;i++){
+        int g,c,d;cin>>g>>c>>d;
+        if(g==1) cnt[3]++;
+        else{
+            if(c==1||c==2) cnt[0]++;
+            else if(c==3) cnt[1]++;
+            else if(c==4) cnt[2]++;
+        }
+    }
+    for(auto e: cnt) cout<<e.second<<'\n';
+}
