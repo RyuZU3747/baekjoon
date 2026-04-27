1d569e68663ee9412c943089593e732a04b8895a
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:56:02 +0900

    [Bronze IV] Title: :chino_shock:, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/27310. ：chino＿shock：/：chino＿shock：.c
+++ 백준/Bronze/27310. ：chino＿shock：/：chino＿shock：.c
@@ -1,12 +1,1 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+s,i;main(){char a[32];gets(a);for(;a[i];)a[i++]==95?s+=6:s++;printf("%d",s+2);}
