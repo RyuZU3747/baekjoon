b8fa1d67ee2030414ff68463a0e47c1a38ec3389
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 02:58:12 +0900

    [Bronze I] Title: 수학 선생님의 고민(Easy), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28237. 수학 선생님의 고민（Easy）/수학 선생님의 고민（Easy）.cc
+++ 백준/Bronze/28237. 수학 선생님의 고민（Easy）/수학 선생님의 고민（Easy）.cc
@@ -1,12 +1,30 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 1000000007
+#define pii pair<int,int>
+#define iii tuple<int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    ll n;cin>>n;
+    for(ll a=1;a<=n;a++){
+        for(ll b=1;b<=n+2;b++){
+            if(n%a==0&&(n+2)%b==0){
+                ll c = n/a;
+                ll d = -(n+2)/b;
+                if(b*c+a*d==n+1){
+                    cout<<a<<' '<<b<<' '<<c<<' '<<d;
+                    exit(0);
+                }
+                else if(-b*c-a*d==n+1){
+                    cout<<a<<' '<<-b<<' '<<c<<' '<<-d;
+                    exit(0);
+                }
+            }
+        }
+    }
+    cout<<-1;
+}
