96b30351a47644d96ec24a6739bc2249b6f6351c
Kim YuKyeom <66157106+RyuZU3747@users.noreply.github.com>
2026-04-28 03:20:32 +0900

    [Gold IV] Title: 고인물의 졸업 계획, Time: 896 ms, Memory: 133220 KB -BaekjoonHub

--- 백준/Gold/28087. 고인물의 졸업 계획/고인물의 졸업 계획.cc
+++ 백준/Gold/28087. 고인물의 졸업 계획/고인물의 졸업 계획.cc
@@ -1,12 +1,33 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+#define ll long long int
+#define swap(a,b) {ll c; c=a;a=b;b=c;}
+#define MOD 998244353
+#define pii pair<int,int>
+#define iii tuple<int,int,int,int>
+#define FASTIO cin.tie(0),ios_base::sync_with_stdio(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    int n,m;cin>>n>>m;
+    vector<pii> arr;
+    vector<int> ans;
+    for(int i=0;i<m;i++){
+        int a;cin>>a;
+        arr.push_back({a,i});
+        if(n<=a&&a<=n*2){
+            cout<<1<<'\n';
+            cout<<i+1<<'\n';
+            exit(0);
+        }
+    }
+    int sum = 0;
+    for(auto e:arr){
+        if(e.first>2*n) continue;
+        sum+=e.first;
+        ans.push_back(e.second);
+        if(sum>=n) break;
+    }
+    cout<<ans.size()<<'\n';
+    for(auto e:ans) cout<<e+1<<'\n';
+}
